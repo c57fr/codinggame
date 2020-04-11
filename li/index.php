032b6978e44ab2e808afa37e73e9cc5d407b29fe
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-11 23:55:35 +0200

    Ini Exo 06 ASCII

--- li/index.php
+++ li/index.php
@@ -12,7 +12,8 @@
       // include 'momo.php';
       // include exos/03_laDescentePOO.php';
       // include 'exos/04_temperatures.php';
-      include 'exos/05_mars.php';
+      // include 'exos/05_mars.php';
+      include 'exos/06_ascii.php';
     ?>
 </body>
 </html>
