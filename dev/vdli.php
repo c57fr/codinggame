1b06909d086e3d02487f28763c3bec4a0cdb77e8
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-11 17:52:52 +0200

    Soluce CodingGame 04 - Temperature

---
+++ dev/vdli.php
@@ -0,0 +1,26 @@
+<?php
+
+function vname()
+{
+  // read backtrace
+  $bt = debug_backtrace(0)[1];
+
+  // var_dump($bt);
+  // read file
+  $file = file($bt['file']);
+  // select exact print_var_name($varname) line
+  $src = $file[$bt['line'] - 1];
+  // search pattern
+  $pat = '#(.*)'.__FUNCTION__.' *?\( *?(.*) *?\)(.*)#i';
+  // extract $varname from match no 2
+  $var = preg_replace($pat, '$2', $src);
+  // return var name
+  return trim($var).' (Ligne '.$bt['line'].'): ';
+}
+
+function vdli($v)
+{
+  $str = '<pre>'.vname($v);
+  $str .= print_r($v, true).'</pre>';
+  echo $str;
+}
--- exos/04_temperatures.php
+++ exos/04_temperatures.php
@@ -1,23 +1,86 @@
 <?php
 
-$inputs  = [-1, -2, -1, 4, 5];
-$n       = count($inputs);
+// https://www.codingame.com/ide/puzzle/temperatures
+
+$inputs = [
+  '0' => -10,
+  '1' => 4,
+  '2' => 4,
+  '3' => -14,
+  '4' => -4,
+];
+// $inputs='';
+$n       = '' !== $inputs ? count($inputs) : 0;
 $nearest = function ($v) {
-  return ['t'=>$v, 'e'=>abs($v)]; // temperature; ecart avec 0
+  return abs($v); // temperature; ecart avec 0
 };
 
-$ecarts = array_map($nearest, $inputs);
+$ecarts = is_array($inputs) ? array_map($nearest, $inputs) : 0;
 
 echo '<pre>';
-print_r(array_values($ecarts['e']));
-echo '</pre><hr>';
-
-echo '<pre>';
-print_r($ecarts);
+vdli($ecarts);
 echo '</pre>';
 
-echo implode(', ',$ecarts).'<hr>';
+// echo implode(', ', $ecarts).'<hr>';
 
-var_dump(min($ecarts));
+//ar_dump(min($ecarts));
 
-echo (array_search(min($ecarts), $ecarts));
+$getTop0 = function ($tab, $refs) {
+  $reps = [];
+  $top0 = min($tab);
+  vdli($top0);
+  foreach ($tab as $k => $v) {
+    if ($v === $top0) {
+      $reps[] =$refs[$k];
+    }
+  }
+  vdli($refs);
+  vdli($reps);
+  
+  return max($reps);
+};
+
+echo '<hr>Température la + proche'.($n > 0) ? $getTop0($ecarts, $inputs) : 0;
+
+//////////////////////////////////////////////////////////////
+/*
+Solution dans codding :
+
+<?php
+
+// $n: the number of temperatures to analyse
+fscanf(STDIN, "%d", $n);
+//error_log(var_export($n, true));
+$inputs = explode(" ", fgets(STDIN));
+//error_log(var_export($inputs, true));
+//error_log(var_export($inputs, true));
+
+for ($i = 0; $i < $n; $i++)
+{
+    $t = intval($inputs[$i]); // a temperature expressed as an integer ranging from -273 to 5526
+// error_log(var_export($t, true));
+    
+}
+$nearest = function ($v) {
+  return abs($v); // temperature; ecart avec 0
+};
+
+$ecarts =is_array($inputs) ? array_map($nearest, $inputs) :0;
+
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+$getTop0 = function ($tab, $refs) {
+  $reps = [];
+  $top0 = min($tab);
+  foreach ($tab as $k => $v) {
+    if ($v === $top0) {
+      $reps[] =$refs[$k];
+    }
+  }
+    return max($reps);
+};
+
+echo (($n>0) ? $getTop0($ecarts, $inputs) : 0) ."\n";
+?>
+*/
