da86645aaf6510d9ad6120bd581e38b909c46537
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-11 03:01:24 +0200

    Fix pour dernier cas (8ème passage)

--- exos/03_laDecentePOO.php
+++ exos/03_laDecentePOO.php
@@ -11,7 +11,6 @@ function vd($tab = 'sommets')
 
 function vds()
 {
-  echo '<hr>';
   vd();
   echo ' - ';
   vd('cibles');
@@ -20,6 +19,7 @@ function vds()
 
 $sommets = [9, 8, 7, 6, 0, 4, 3, 2];
 $cibles  = $sommets;
+array_push($cibles, 0);
 // unset($cibles[5]);
 
 /*
@@ -33,7 +33,6 @@ Height of mountain 6 : 3
 Height of mountain 7 : 2
 */
 
-vd(); echo '<hr>';
 // => Sans une boucle for, $sommets[$i] va te sortir la haute de la montagne en cours
 // Soit l'équivalent dans coding, de :
 /*
@@ -61,23 +60,17 @@ class Culminant extends Montagne
 // $montagne  = new Montagne();
 $culminant = new Culminant();
 
-foreach ($sommets as $k => $s) { // Faut faire autant de passage qu'il y a de sommets à détruire
+foreach ($sommets as $p => $k) { // Faut faire autant de passage qu'il y a de sommets à détruire
   $culminant->h = 0; // On re-initialise la hauteur du culminant pour un nouveau passage
 
   foreach ($cibles as $i => $h) {
-    if (7 === $k) {
-      echo 'der ('.$i.'-'.$h.')<hr>';
-    }
-    // $montagne->h = $h;
-    // echo $sommets[$i].$montagne->h;
-    // echo '<strong>'.$i.'->'.$h.'</strong><br>';
-    // echo $i;
     if ($h >= $culminant->h) {
-      echo "(${h} - (${i},{$culminant->i}) )";
-      $culminant->update($i, $h);
-      echo 'ok:'.$cibles[array_search($h, $sommets, true)];
-      unset($cibles[$i]);
+      $nouvoSommetMax = array_search($h, $sommets, true);
+      $culminant->update($nouvoSommetMax, $h);
+      
+      unset($cibles[$i]); // Destruction montagne d'index $i
     }
   }
-  echo '<br>=> Pour le passage '.($k + 1).": Index <strong>{$culminant->i}</strong> à détruire \n<br><br>"; // The index of the mountain to fire on.
+  echo '<br>=> Pour le passage '.($p + 1).": Index <strong>{$culminant->i}</strong> à détruire (Soit la montagne de hauteur {$culminant->h} )\n<br><br>"; // The index of the mountain to fire on.
+  vds();
 }
