8a4bf089cbf95d410664cbfd97e28ab7540e8ef6
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-11 02:14:39 +0200

    Corr lecture du tablo

--- montagne.php
+++ montagne.php
@@ -1,5 +1,13 @@
 <?php
+
 $sommets = [9, 8, 7, 6, 0, 4, 3, 2];
-for ($i=0; $i <$sommets ; $i++) { 
-    echo $sommets;
+for ($i = 0; $i < count($sommets); ++$i) {
+  echo $sommets[$i];
+}
+
+echo '<hr>';
+// Une façon préférable:
+
+foreach ($sommets as $i=>$h){
+    echo $sommets[$i];
 }
