875da34998cd72ec9e6b426ec9a3d0a34642839d
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-12 14:35:14 +0200

    Up LaDecente Mo

---
+++ mo/exos/03_laDecente.php
@@ -0,0 +1,40 @@
+<?php
+
+/*$sommets = [9, 8, 7, 6, 0, 4, 3, 2];
+for ($i = 0; $i < count($sommets); ++$i) {
+  echo $sommets[$i];
+}
+
+echo '<hr>';
+// Une façon préférable:
+
+foreach ($sommets as $i=>$h){
+    echo $sommets[$i];
+}
+
+echo '<hr>';
+
+$sommets = [9,8,7,6,0,4,3,2];
+for ($i = 0; $i < count($sommets); ++$i){
+  echo $sommets[$i];
+}*/
+
+
+$sommets = [9, 8, 7, 6, 0, 4, 3, 2];
+
+// On a des sommets... On itère dessus pour les afficher dans l'ordre qu'ils sont...
+
+foreach ($sommets as $i => $h) {
+  echo $sommets[$i]."<br/>";
+}
+
+//////////////////////////////////////////////////////////////
+/*
+Solution dans codding :
+
+
+<?php
+
+
+
+*/
