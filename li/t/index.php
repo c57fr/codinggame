f2ed765a26eb4b4324aca09d8ac5f58e56608a29
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-13 01:17:06 +0200

    double un array avec preg_replace_callback()

--- li/t/index.php
+++ li/t/index.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test</title>
+    <style>
+     body {
+       font-family: arial;
+     }
+    </style>
 </head>
 <body>
     <?php
--- li/t/t.php
+++ li/t/t.php
@@ -36,7 +36,7 @@ echo '<hr>';
 
 echo preg_replace_callback('@.@', function ($m) {
   return sprintf('%07b', ord($m[0]));
-}, 'CC');
+}, '%');
 
 // echo rtrim(
 //   preg_replace_callback('@(.)\1*@', function ($m) {
@@ -45,3 +45,18 @@ echo preg_replace_callback('@.@', function ($m) {
 //   preg_replace_callback('@.@', function ($m) {
 //     return sprintf('%07b', ord($m[0]));
 //   }, $MESSAGE)));
+
+echo '<hr>';
+
+$values = [2, 3, 17, 4, 5];
+
+function double($v)
+{
+  return ($v[0] * 2).' ';
+}
+// vdli($values);
+
+// Doc: https://medium.com/factory-mind/regex-tutorial-a-simple-cheatsheet-by-examples-649dc1c3f285
+
+// https://regex101.com/r/94TnZp/1
+echo '$values = ['.implode(',', $values).'] =>'.preg_replace_callback('@([^,]+)@', 'double', implode(',', $values));
