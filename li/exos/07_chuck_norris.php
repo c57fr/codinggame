f0add0a9536fba1c1594d2e82e9509c386a3553b
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-12 23:40:57 +0200

    Optimisation 07 ASCII (Chuck Norris)

--- li/exos/07_chuck_norris.php
+++ li/exos/07_chuck_norris.php
@@ -4,41 +4,55 @@ Exo 07: Chuck NORRIS.<hr>
 
 // https://www.codingame.com/training/easy/chuck-norris
 
-$MESSAGE = 'C';
+$MESSAGE = 'CC';
 
-$carbin = decbin(ord($MESSAGE));
-// $len     = strlen($carbin);
-// vdli($len);
-// $carbin = '456';
+$cesar = function ($MESSAGE) {
+  $carbin = '';
+  $msg    = str_split($MESSAGE);
+  foreach ($msg as $let) {
+    $carbin .= decbin(ord($let));
+    vdli($carbin);
+    $carbin = str_pad($carbin, 7, '0',STR_PAD_LEFT);
+    vdli($carbin);
+  }
+  // $len     = strlen($carbin);
+  // vdli($len);
+  // $carbin = '456';
+  // var_dump($carbin);
+  // vdli($carbin);
+  $carbin = str_replace('01', '0x1', $carbin);
+  $carbin = str_replace('10', '1x0', $carbin);
+  // echo '<br>';
+  // var_dump($carbin);
+  // echo '<br>';
+  // $carbin = '1x0000x11';
+  // var_dump($carbin);
+  // exit;
 
-var_dump($carbin);
+  $bbins = explode('x', $carbin);
+  // vdli($bbins);
+  $rep = '';
 
-$carbin=str_replace('01', '0x1',$carbin);
-$carbin=str_replace('10', '1x0',$carbin);
+  foreach ($bbins as $i => $v) {
+    // vdli($v);
+    $rep .= (($v[0] * 1) ? ' 0 ' : ' 00 ').str_repeat('0', strlen($v)).' ';
+  }
 
+  return trim($rep);
+};
 
-// echo '<br>';
-// var_dump($carbin);
-// echo '<br>';
-// $carbin = '1x0000x11';
-// var_dump($carbin);
-// exit;
+// $tab = str_split($MESSAGE);
+// vdli($tab);
 
-$bbins = explode('x', $carbin);
-vdli($bbins);
+echo $cesar($MESSAGE)."\n";
 
-// $car1 = array_shift($bins);
-// $car1 = $bins[0];
-// vdli($car1);
-
-foreach ($bbins as $i => $v) {
-  echo trim((($v[0] * 1) ? '0 ' : '00 ').str_repeat('0', strlen($v)).' ');
-}
+// echo '<hr>';
+// var_dump('0 0 00 0000 0 000 00 0000 0 00');
 
 //////////////////////////////////////////////////////////////
 
 /*
-Solution dans codding :
+Solution dans codding : Même code sauf $MESSAGE = 'CC';
 
 <?php
 
