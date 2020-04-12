73fdc6484c6f2d306f0598c3135664c20c5e54e6
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-12 21:30:51 +0200

    1ere piste codage

--- li/exos/07_chuck_norris.php
+++ li/exos/07_chuck_norris.php
@@ -4,9 +4,24 @@ Exo 07: Chuck NORRIS.<hr>
 
 // https://www.codingame.com/training/easy/chuck-norris
 
+$MESSAGE = 'V';
 
+$carbin = decbin(ord($MESSAGE));
+// $len     = strlen($carbin);
+// vdli($len);
+// $carbin = '456';
+var_dump($carbin);
 
+$bins = str_split($carbin);
+vdli($bins);
 
+$car1 = array_shift($bins);
+vdli($car1);
+vdli($bins);
+
+// foreach ($carbin as $k => $v) {
+//   echo $k;
+// }
 
 //////////////////////////////////////////////////////////////
 
