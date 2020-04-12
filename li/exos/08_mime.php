4772add73eb8cb1ef8f612db9b9256303e7ad561
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-13 01:50:14 +0200

    08 Mime Type

---
+++ li/exos/08_mime.php
@@ -0,0 +1,20 @@
+Exo 07: Mime Type.<hr>
+
+<?php
+
+// https://www.codingame.com/training/easy/mime-type
+
+
+
+
+//////////////////////////////////////////////////////////////
+
+/*
+Solution dans codding : Mêmes codes sauf $MESSAGE = 'CC';
+
+<?php
+
+
+
+
+*/
