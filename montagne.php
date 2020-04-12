9d594994f03b3ea831f2a1f840137aa437bce7dc
Momo <seckndanane16@gmail.com>
2020-04-12 03:42:47 +0200

    reprise de l'exo de la page montagne

--- montagne.php
+++ montagne.php
@@ -1,6 +1,6 @@
 <?php
 
-$sommets = [9, 8, 7, 6, 0, 4, 3, 2];
+/*$sommets = [9, 8, 7, 6, 0, 4, 3, 2];
 for ($i = 0; $i < count($sommets); ++$i) {
   echo $sommets[$i];
 }
@@ -10,4 +10,15 @@ echo '<hr>';
 
 foreach ($sommets as $i=>$h){
     echo $sommets[$i];
+}
+
+echo '<hr>';
+
+$sommets = [9,8,7,6,0,4,3,2];
+for ($i = 0; $i < count($sommets); ++$i){
+  echo $sommets[$i];
+}*/
+$sommets = [9, 8, 7, 6, 0, 4, 3, 2];
+foreach ($sommets as $i => $h) {
+  echo $sommets[$i]."<br/>";
 }
