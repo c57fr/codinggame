f66f0e5a6b5224212495686881550bf7a82bc0a0
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-13 13:26:57 +0200

    Data TR 09

--- li/exos/09_defibrillateurs.php
+++ li/exos/09_defibrillateurs.php
@@ -4,7 +4,15 @@ Exo 09: Défibrillateurs.<hr>
 
 https://www.codingame.com/training/easy/defibrillators
 
-
+// Reconstitution des données du test 1
+$LON   = '3,879483';
+$LAT   = '43,608177';
+$N     = 3;
+$DEFIB = [
+  '0 : 1;Maison de la Prevention Sante;6 rue Maguelone 340000 Montpellier;;3,87952263361082;43,6071285339217',
+  '1 : 2;Hotel de Ville;1 place Georges Freche 34267 Montpellier;;3,89652239197876;43,5987299452849',
+  '2 : 3;Zoo de Lunaret;50 avenue Agropolis 34090 Mtp;;3,87388031141133;43,6395872778854',
+];
 
 //////////////////////////////////////////////////////////////
 
