deb058cbc9437103b5122c5e161f2af911dce5d4
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-16 12:07:14 +0200

    Reconstitution Succession

--- li/exos/011_succession.php
+++ li/exos/011_succession.php
@@ -4,8 +4,68 @@ Exo 011: Order of Succession.<hr>
 
 // Reconstitution des données du test 1
 
-$d = 1;
-vdli($d);
+$ms = [
+  [
+    0 => 'Elizabeth',
+    1 => '-',
+    2 => 1926,
+    3 => '-',
+    4 => 'Anglican',
+    5 => 'F',
+  ],
+  [
+    0 => 'Charles',
+    1 => 'Elizabeth',
+    2 => 1948,
+    3 => '-',
+    4 => 'Anglican',
+    5 => 'M',
+  ],
+  [
+    0 => 'William',
+    1 => 'Charles',
+    2 => 1982,
+    3 => '-',
+    4 => 'Anglican',
+    5 => 'M',
+  ],
+  [
+    0 => 'George',
+    1 => 'William',
+    2 => 2013,
+    3 => '-',
+    4 => 'Anglican',
+    5 => 'M',
+  ],
+  [
+    0 => 'Charlotte',
+    1 => 'William',
+    2 => 2015,
+    3 => '-',
+    4 => 'Anglican',
+    5 => 'F',
+  ],
+  [
+    0 => 'Henry',
+    1 => 'Charles',
+    2 => 1984,
+    3 => '-',
+    4 => 'Anglican',
+    5 => 'M',
+  ],
+];
+
+$ms[0]['bg'] = 1;
+$ms[0]['bd'] = 2;
+
+foreach ($ms as $k => $v) {
+  $ms['noms'][] = $v[0];
+}
+
+vdli($ms['noms']);
+foreach ($ms as $k => $v) {
+   vdli([$v[0], $v['bg']??'', $v['bd']??'']);
+  }
 //////////////////////////////////////////////////////////////
 /*
 Solution dans codding : Même code
