a0f4f261d462850146313c4a05e2a10a7f017860
Momo <seckndanane16@gmail.com>
2020-04-19 18:24:30 +0200

    fonction helloWord

---
+++ mo/codingGame/exo1_function_helloWorld.php
@@ -0,0 +1,8 @@
+<?php
+//Faite en sorte que la fonction HelloWorld retourne exactement la valeur Hello World!
+
+function HelloWorld(){
+    $c = "Hello World";
+    return $c;
+}
+echo HelloWorld();
