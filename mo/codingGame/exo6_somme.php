e20036821e1f71d23ae744ec28a95bb2c00fe36b
Momo <seckndanane16@gmail.com>
2020-04-20 04:26:55 +0200

    somme

---
+++ mo/codingGame/exo6_somme.php
@@ -0,0 +1,10 @@
+<?php
+// Créer une fonction from scratch qui s'appelle somme(). Elle prendra deux arguments de type int. Elle devra retourner la somme des deux. Exemple : argument 1 = 5 Argument 2 = 5 ; Resultat : 10
+
+function somme(){
+    $argument1 = 5;
+    $argument2 = 5;
+
+    return $argument1 + $argument2;
+}
+echo somme();
