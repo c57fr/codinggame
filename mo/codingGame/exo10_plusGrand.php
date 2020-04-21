2a43fca0c54a153ba38de97dec67ea68871c60a3
Momo <seckndanane16@gmail.com>
2020-04-21 03:12:07 +0200

    plus grand

---
+++ mo/codingGame/exo10_plusGrand.php
@@ -0,0 +1,13 @@
+<?php
+
+
+function plusGrand($argument1, $argument2){
+    if($argument1 > $argument2)
+    {
+        return $argument1;
+        //return "$argument1 est plus grand que $argument2";
+    }
+    //return "$argument1 est plus petit que $argument2";
+}
+//var_dump(plusGrand(23,15));
+echo plusGrand(23,15);
