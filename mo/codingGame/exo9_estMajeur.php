9ddfe2db3b0989b0a637cda7afe997499e3fb01a
Momo <seckndanane16@gmail.com>
2020-04-21 02:46:30 +0200

    corrigé de l'exo sur la majorité

--- mo/codingGame/exo9_estMajeur.php
+++ mo/codingGame/exo9_estMajeur.php
@@ -1,7 +1,7 @@
 <?php
 // Créer une fonction from scratch qui s'appelle estIlMajeure(). Elle prendra un argument de type int. Elle devra retourner un boolean. Si age >= 18 elle doit retourner true si age < 18 elle doit retourner false Exemple : age = 5 ==> false age = 34 ==> true
 
-function estIlMajeure(){
+/*function estIlMajeure(){
     $age = 32;
     if($age >= 18)
     {
@@ -26,3 +26,13 @@ function Majeure(){
     return $age;
 }
 echo Majeure();
+echo "<hr>";*/
+function estIlMajeure($age){
+    if($age >= 18)
+    {
+        return true;
+    }
+    return false;
+}
+var_dump(estIlMajeure(16));
+
