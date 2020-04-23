ab1874acb59c35270a6de5e44a2d8056ed32bd38
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-23 05:40:58 +0200

    Corr 008 - Mo

--- mo/codingGame/008_multiplication.php
+++ mo/codingGame/008_multiplication.php
@@ -1,10 +1,10 @@
 <?php
+
+function multiplication($a, $b)
+{
+  return $a * $b;
+}
+
 // Créer une fonction from scratch qui s'appelle multiplication(). Elle prendra deux arguments de type int. Elle devra retourner la multiplication des deux. Exemple : argument 1 = 5 Argument 2 = 5 ; Resultat : 25
 
-function multiplication(){
-    $argument1 = 5;
-    $argument2 = 5;
-
-    return $argument1 * $argument2;
-}
-echo multiplication();
+echo multiplication(5, 5);
