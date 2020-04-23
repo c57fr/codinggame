0682b2ec10220444ca0dbd9ef83ab7e827d84dfd
Momo <seckndanane16@gmail.com>
2020-04-24 01:41:58 +0200

    plus petit du tableau

--- mo/codingGame/015_plusGrand.php
+++ mo/codingGame/015_plusGrand.php
@@ -2,10 +2,13 @@
   // Créer une fonction from scratch qui s'appelle plusGrand(). Elle prendra un argument de type array. Elle devra retourner le plus grand des élements présent dans l'array. Si l'array est vide, il faudra retourner null;
 
 function plusGrand($notes){
-    if(is_array($notes))
+    if($notes)
     {
         return max($notes);
     }
+    return null;
 }
-$arr = [32, 56];
+$arr = [32, 56, 78];
 var_dump(plusGrand($arr));
+echo '<hr>';
+var_dump(plusGrand([]));
