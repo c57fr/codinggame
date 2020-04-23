ff2a8e81e913322073d87709eac8f7528ae5037c
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-23 04:14:54 +0200

    Add URL de l'exo sur Codingame

--- mo/codingGame/014_dernierElementTableau copy.php
+++ mo/codingGame/014_dernierElementTableau copy.php
@@ -4,23 +4,23 @@ if (!function_exists('vdli')) {
   include '../../dev/vdli.php';
 } // vdli()
 
+// https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
+
+
 //  Créer une fonction from scratch qui s'appelle dernierElementTableau(). Elle prendra un argument de type array. Elle devra retourner le premier élement du tableau. Si l'array est vide, il faudra retourner null;
 
 /**
- * Retourne le dernier élément d'un tableau
- * 
+ * Retourne le dernier élément d'un tableau.
+ *
  * $notes: Le tableau des notes
  */
-function dernierElementTableau(Array $notes): ?int
+function dernierElementTableau(array $notes): ?int
 {
   return $notes[0] ?? null; // Changer ici
 }
-//var_dump(premierElementTableau(4,78));
 
 $arr = [78, 9];
 vdli(dernierElementTableau($arr));
 echo '<hr>';
 $arr = [];
 var_dump(dernierElementTableau($arr));
-
-
