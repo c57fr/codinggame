2ee7e148ba6f860caf1dd15670a1ff30649bcc3a
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-23 05:31:31 +0200

    Corr 004 - Mo

--- mo/codingGame/004_concatenation.php
+++ mo/codingGame/004_concatenation.php
@@ -1,12 +1,8 @@
 <?php
-// Créer une fonction from scratch qui s'appelle concatenation(). Elle prendra deux arguments de type string. Elle devra retourner la concatenation des deux. Exemple : argument 1 = Antoine Argument 2 = Griezmann; Resultat : AntoineGriezmann
 
-function concatenation(){
-    $argument1 = "Antoine";
-    $argument2 = "Griezmann";
-
-    return $argument1.$argument2;
+function concatenation($s1, $s2)
+{
+  return $s1.$s2;
 }
 
-echo concatenation();
-
+echo concatenation('Antoine', 'Griezman');
