3b133773ad621e95f2f3e32484f487a7d2b86915
GrCOTE7 <GrCOTE7@GMail.com>
2020-04-23 05:26:46 +0200

    Corr 002 - Mo

--- mo/codingGame/002_meilleurProf.php
+++ mo/codingGame/002_meilleurProf.php
@@ -1,16 +1,7 @@
 <?php
-// Créer une fonction from scratch qui s'appelle quiEstLeMeilleurProf(). Elle doit retourner Le prof de programmation Web
 
-function quiEstLeMeilleurProf(){
-    return "Le prof de programmation Web";
+function quiEstLeMeilleurProf()
+{
+  return 'Le prof de programmation Web';
 }
 echo quiEstLeMeilleurProf();
-
-echo "<hr>";
-
-// deuxieme maniere d'ecrire
-function bestProf(){
-    $c = "Le prof de programmation Web";
-    return $c;
-}
-echo bestProf();
