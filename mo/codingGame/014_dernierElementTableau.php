28c3de38d4d65f57c823781cc646e7f63e5064ad
Momo <seckndanane16@gmail.com>
2020-04-23 20:32:41 +0200

    dernier élément du tableau

--- mo/codingGame/014_dernierElementTableau.php
+++ mo/codingGame/014_dernierElementTableau.php
@@ -13,7 +13,7 @@ if (!function_exists('vdli')) {
  *
  * $notes: Le tableau des notes
  */
-function dernierElementTableau(array $notes): ?int
+/*function dernierElementTableau(array $notes): ?int
 {
   return $notes[0] ?? null; // Changer ici
 }
@@ -22,4 +22,16 @@ $arr = [78, 9];
 vdli(dernierElementTableau($arr));
 echo '<hr>';
 $arr = [];
-var_dump(dernierElementTableau($arr));
+var_dump(dernierElementTableau($arr));*/
+function dernierElementTableau($notes){
+  if(!empty($notes))
+  {
+    return array_pop($notes);
+  }
+  return null;
+
+}
+
+var_dump(dernierElementTableau([12, 34, 5]));
+echo '<hr>';
+var_dump(dernierElementTableau([]));
