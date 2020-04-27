b22e18e310c054a3c14abe5850933197ebd600f6
GC7 <GrCOTE7@Gmail.com>
2020-04-27 02:18:41 +0200

    Enfants

--- li/exos/011_affHierarchieRecursive.php
+++ li/exos/011_affHierarchieRecursive.php
@@ -5,14 +5,19 @@ $rens = function ($u) {
   // vdli($u);
   return
   '<td style="padding-right: 10px;">'.
-    (($u[6]) ? str_repeat('&nbsp;', ($u[7] * 6)).' └─ ' : str_repeat('&nbsp;', 7)).
-    $u[6].' <strong>'.$u[0].'</strong><td>'.$u[8].'</td> <td style="padding-left: 10px; padding-right: 10px;">'.$u[2].' </td><td style="padding-left: 10px; padding-right: 10px;"> P '.$u[7].'</td>';
+  (($u[6]) ? str_repeat('&nbsp;', ($u[7] * 6)).' └─ ' : str_repeat('&nbsp;', 7)).
+  $u[6].' <strong>'.$u[0].'</strong><td>'.$u[8].'</td> <td style="padding-left: 10px; padding-right: 10px;">'.$u[2].' </td><td style="padding-left: 10px; padding-right: 10px;"> P '.$u[7].'</td>';
 };
 
 echo '<table border=0><tr>';
-for ($i = 0; $i < $n; ++$i) {
+// for ($i = 0; $i < $n; ++$i) {
   //TODO Moulinette pour gérer filleuls
-  echo '<tr><td>'.$i.'</td>'.$rens($ms[$i]).'</tr>';
-}
+  $i = 0;
+//   echo '<tr><td>'.$i.'</td>'.$rens($ms[$i]).'</tr>';
+  $idps         = array_column($ms, 8);
+  vdli(array_keys($idps, 0, true)); // get enfants de id 0
+  
+// }
 echo '</table>';
+
 // ALT + 192 & ALT + 196
