581b9f50382d63e066d011a77e23b368476fc61e
GC7 <GrCOTE7@Gmail.com>
2020-04-27 18:43:58 +0200

    Ini 012 - Tableur

---
+++ li/exos/012_spreadsheet.php
@@ -0,0 +1,21 @@
+Exo 012: Spreadsheet (Tableur).
+<hr>
+<?php
+if (!function_exists('vdli')) include '../../dev/vdli.php';
+
+//https://www.codingame.com/training/easy/1d-spreadsheet
+
+// Reconstitution des données du test 1
+
+
+
+//////////////////////////////////////////////////////////////
+/*
+Solution dans codding :
+
+<?php
+
+
+
+
+*/
