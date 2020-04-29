523fdfabe14462cabd33106593e0cf331b5016a3
GC7 <GrCOTE7@Gmail.com>
2020-04-29 03:44:41 +0200

    Exemples transtypage

--- mo/codingGame/17_verificationPassword.php
+++ mo/codingGame/17_verificationPassword.php
@@ -1,7 +1,26 @@
 <?php
 
-function verificationPassword ($str){
-    return (bool) (strlen($str)>7);
+if (!function_exists('vdli')) {
+  include '../../dev/vdli.php';
+} // vdli()
+
+// https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
+
+// Créer une fonction from scratch qui s'appelle verificationPassword(). Elle prendra un argument de type string. Elle devra retourner un boolean qui vaut true si le password fait au moins 8 caractères et false si moins.
+
+$a = '7';
+
+vdli(gettype($a)); // Là, c'est str
+
+vdli(gettype((int) $a)); // Là, c'est integer
+
+
+
+exit;
+
+function verificationPassword($str)
+{
+  return (bool) (strlen($str) > 7);
 }
 
 $mdp1 = 'mine';
@@ -9,8 +28,3 @@ $mdp2 = 'mouhamadou';
 var_dump(verificationPassword($mdp1));
 echo '<hr>';
 var_dump(verificationPassword($mdp2));
-
-
-// https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
-
-// Créer une fonction from scratch qui s'appelle verificationPassword(). Elle prendra un argument de type string. Elle devra retourner un boolean qui vaut true si le password fait au moins 8 caractères et false si moins.
