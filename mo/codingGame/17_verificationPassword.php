b542e2e34afef4e8ce299b6e95dcc2aec08b32a4
Momo <seckndanane16@gmail.com>
2020-04-29 04:05:06 +0200

    où mettre :e push dans cette version

--- mo/codingGame/17_verificationPassword.php
+++ mo/codingGame/17_verificationPassword.php
@@ -1,6 +1,6 @@
 <?php
 
-function verificationPassword ($str){
+/*function verificationPassword ($str){
     return (bool) (strlen($str)>7);
 }
 
@@ -8,9 +8,23 @@ $mdp1 = 'mine';
 $mdp2 = 'mouhamadou';
 var_dump(verificationPassword($mdp1));
 echo '<hr>';
-var_dump(verificationPassword($mdp2));
+var_dump(verificationPassword($mdp2));*/
 
 
 // https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
 
 // Créer une fonction from scratch qui s'appelle verificationPassword(). Elle prendra un argument de type string. Elle devra retourner un boolean qui vaut true si le password fait au moins 8 caractères et false si moins.
+
+function verificationPassword($motDePasse)
+{
+    if(strlen($motDePasse >= 8))
+    {
+        return true;
+    }
+    return false;
+}
+$mdp1 = 'mine';
+$mdp2 = 'mouhamadou';
+var_dump(verificationPassword($mdp1));
+echo '<hr>';
+var_dump(verificationPassword($mdp2));
