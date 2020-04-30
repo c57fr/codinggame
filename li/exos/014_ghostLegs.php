d50fe635400541a59d1241e2d17b33159d5b1928
GC7 <GrCOTE7@Gmail.com>
2020-04-30 02:46:33 +0200

    Add 2do en POO

--- li/exos/014_ghostLegs.php
+++ li/exos/014_ghostLegs.php
@@ -31,67 +31,75 @@ foreach ($lines as $k => $l) {
 }
 echo '</pre>';
 
-// Recxherche des T
+// S1 Procédural
+
+// S1 en POO
+
+// S2 en POO
+
+// S2 en POO
 
 /*
-Solution dans codding :
-
-<?php
-
-
 public class Solution
 {
   public static void Main()
   {
-    //Read diagram size.
-    string[] line = Console.ReadLine().Split();
-    int W = int.Parse(line[0]);
-    int H = int.Parse(line[1]);
+  //Read diagram size.
+  string[] line = Console.ReadLine().Split();
+  int W = int.Parse(line[0]);
+  int H = int.Parse(line[1]);
 
-    //Read top labels and create start indices.
-    string[] T = Console.ReadLine().Split(new [] { "  " }, StringSplitOptions.None);
-    int[] Ti = new int[T.Length];
+  //Read top labels and create start indices.
+  string[] T = Console.ReadLine().Split(new [] { "  " }, StringSplitOptions.None);
+  int[] Ti = new int[T.Length];
 
-    for (int i = 0; i < Ti.Length; i++)
+  for (int i = 0; i < Ti.Length; i++)
+  {
+    Ti[i] = i;
+  }
+
+  //Read diagram lines.
+  for (int i = 0; i < H - 2; i++)
+  {
+    line = Console.ReadLine().Split('|');
+
+    //Search for horizontal lines.
+    for (int j = 0; j < line.Length; j++)
     {
-      Ti[i] = i;
-    }
-
-    //Read diagram lines.
-    for (int i = 0; i < H - 2; i++)
+    if (line[j] == "--")
     {
-      line = Console.ReadLine().Split('|');
-
-      //Search for horizontal lines.
-      for (int j = 0; j < line.Length; j++)
+      //Check where the horizontal line is and change indices.
+      for (int k = 0; k < Ti.Length; k++)
       {
-        if (line[j] == "--")
-        {
-          //Check where the horizontal line is and change indices.
-          for (int k = 0; k < Ti.Length; k++)
-          {
-            if (Ti[k] == j - 1)
-            {
-              Ti[k] += 1;
-            }
-            else if (Ti[k] == j)
-            {
-              Ti[k] -= 1;
-            }
-          }
-        }
+      if (Ti[k] == j - 1)
+      {
+        Ti[k] += 1;
+      }
+      else if (Ti[k] == j)
+      {
+        Ti[k] -= 1;
+      }
       }
     }
-
-    //Read bottom labels.
-    string[] B = Console.ReadLine().Split(new [] { "  " }, StringSplitOptions.None);
-
-    //Print result.
-    for (int i = 0; i < T.Length; i++)
-    {
-      Console.WriteLine(T[i] + B[Ti[i]]);
     }
   }
+
+  //Read bottom labels.
+  string[] B = Console.ReadLine().Split(new [] { "  " }, StringSplitOptions.None);
+
+  //Print result.
+  for (int i = 0; i < T.Length; i++)
+  {
+    Console.WriteLine(T[i] + B[Ti[i]]);
+  }
+  }
 }
+
+##############################################################################
+
+Solution dans codding :
+
+<?php
+
 
 */
