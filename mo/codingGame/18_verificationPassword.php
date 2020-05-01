a1d66a4240c501549c18d86dcf815e7e28d4c7ce
GC7 <GrCOTE7@Gmail.com>
2020-05-01 20:02:28 +0200

    Amélioration exo18 Mo

--- mo/codingGame/18_verificationPassword.php
+++ mo/codingGame/18_verificationPassword.php
@@ -1,4 +1,61 @@
 <?php
+
+$verificationPasswordMo = function ($mp) { // Soluce Mo
+  $errs = [];
+
+  $err = 'Votre mot de passe doit avoir au ';
+  if (strlen($mp) < 8) {
+    $errs[] = 'Votre mot de passe doit avoir au moins 8 caractères';
+  }
+
+  if (!preg_match('/[0-9]/', $mp)) {
+    $errs[] = 'Votre mot de passe doit avoir au moins un chiffre';
+  }
+
+  if (!preg_match('/[A-Z]/', $mp)) {
+    $errs[] = 'Votre mot de passe doit avoir au moins 1 majuscule';
+  }
+
+  if (!preg_match('/[a-z]/', $mp)) {
+    $errs[] = 'Votre mot de passe doit avoir au moins 1 minuscule';
+  }
+  // [%10s]\n
+  $sps = str_repeat('&nbsp;', 3);
+
+  return  (count($errs)) ? (sprintf('%s %s %s', '<p>', $sps.'- '.implode((',<br>'.$sps.' - '), $errs), '.</p>')) : 'Correct.';
+};
+
+$verificationPasswordLi = function ($mp) { // Soluce GC7
+  return preg_match('/(?=.*[A-Z]+).(?=.*[a-z]+).(?=.*[0-9]+).{6,}/', $mp);
+};
+
+// @i Moins précise que ta fonction, elle a le mérite d'être concise, et de retourner précisément ce que demande l'exo... ;-)... Mais BRAVO: Le principe est dans la tienne aussi :-)
+
+// Différents mp pour tests
+$mps = [
+  'mine',
+  '1234567',
+  '12345678',
+  'abc',
+  'abcdefg',
+  'ABCDEFG',
+  'ABCDEFGH',
+  'abcABC',
+  'abcdABCD',
+  'mouhaMadou1', // Le seul qui a 8+ car, 1+ min et 1+ maj
+];
+$codeurs = ['Mo',  'Li'];
+$codeurs = array_reverse($codeurs);
+
+foreach ($codeurs as $k => $codeur) {
+  echo  '<h2>'.$codeur.'</h2>';
+
+  foreach ($mps as $mp) {
+    print_r('<strong>'.$mp.'</strong> : '.${'verificationPassword'.$codeur}($mp).'<br>');
+  }
+  echo (!$k) ? '<br><hr>' : ''; // (!$k) = Après le premier codeur
+}
+
 /*
 Créer une fonction from scratch qui s'appelle verificationPassword(). Elle prendra un argument de type string. Elle devra retourner un boolean qui vaut true si le password respecte les règles suivantes :
 
@@ -6,30 +63,3 @@ Faire au moins 8 caractères
 Avoir au moins 1 chiffre
 Avoir au moins une majuscule et une minuscule
 */
-function verificationPassword($motDePasse){
-    $error = [];
-    if((strlen($motDePasse)) >= 8)
-    {
-        if(preg_match('/(?=.*[0-9])/', $motDePasse))
-        {
-            if(preg_match('/(?=.*[a-z])/', $motDePasse))
-            {
-                if(preg_match('/(?=.*[A-Z])/', $motDePasse))
-                {
-                    return true;
-                }else{
-                    return $error = "Votre mot de passe doit avoir au moins 1 majuscule";
-                }
-            }else {
-                return $error = "Votre mot de passe doit avoir au moins 1 minuscule";
-            }
-        }else{
-            return $error = "Votre mot de passe doit avoir au moins un chiffre";
-        }
-    }else {
-        return $error = "Votre mot de passe doit avoir plus de 7 chiffres";
-    }
-}
-$mdp1 = 'mine';
-$mdp2 = 'mouhaMadou1';
-var_dump(verificationPassword($mdp2));
