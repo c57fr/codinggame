b511802749ef62fa6520da6395a4871d6cf00bb0
GC7 <GrCOTE7@Gmail.com>
2020-05-01 21:29:49 +0200

    Fix colors 2do & comments

---
+++ li/t/vsc_2do_colors.php
@@ -0,0 +1,32 @@
+<?php
+
+echo 'Définition des couleurs de l\'éditeur VSC (Todo Tree)';
+
+// 2do À mettre ds c57.fr
+// 2do Y compris settings.json de VSC (Win)
+
+// Implique une action à faire
+// d'urgence (Max 21)
+// 2 dbug À débuger     (rouge vif)
+// 2 fix  À rêgler      (Jaune vif)
+// -----------------
+// 2 do   À faire       (orange vif)
+// dès que possible
+// -----------------
+// 2 ar   À améliorer   (cyan)
+//        À revoir
+// dès que plus de 2do
+
+// Ceux qui n'entraînes pas d'action nécessaire
+
+// Simple commentaire :
+// !      Très important, danger, impératif
+// @bug   Bug, défaillance sans gravité
+//        (Arranger à l'occase)
+// @not   À noter (Important)
+// @i     Info, précision (Majeure)
+// ***    Info
+// @q ok  Question importante
+// ? sdfs Question
+// @f     Futur
+// @s     Signal
