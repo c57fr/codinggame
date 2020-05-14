9c1584e1ee1d4ead93fcb060255ff48fdd4b52e0
Momo <seckndanane16@gmail.com>
2020-05-14 03:43:28 +0200

    exo 19 corrigé

--- mo/codingGame/19_capital.php
+++ mo/codingGame/19_capital.php
@@ -1,28 +1,33 @@
 <?php
 // Créer une fonction from scratch qui s'appelle capital(). Elle prendra un argument de type string. Elle devra retourner le nom de la capitale des pays suivants :
-function capital($capital){
-    Switch($capital) {
-        case "France":
-            return "Paris";
-        break;
-        case "Allemagne":
-            return "Berlin";
-        break;
-        case"Italie":
-            return "Rome";
-        break;
-        case  "Maroc":
-            return "Rabat";
-        break;
-        case "Portugal":
-            return "Lisbone";
-        break;
-        case "Angleterre":
-            return "Londres";
-        break;
-        case "Tout autre pays":
-            return "Inconnu";
-    }
-}
 
+function capital($str)
+{
+  switch ($str) {
+    case 'France':
+        return 'Paris';
+        break;
+    case 'Allemagne':
+        return 'Berlin';
+        break;
+    case 'Italie':
+        return 'Rome';
+        break;
+    case 'Maroc':
+        return 'Rabat';
+        break;
+    case 'Espagne':
+        return 'Madrid';
+        break;
+    case 'Portugal':
+        return 'Lisbonne';
+        break;
+    case 'Angleterre':
+        return 'Londres';
+        break;
+        default:
+        return 'Inconnu';
+        break;
+}
+}
 var_dump(capital('Maroc'));
