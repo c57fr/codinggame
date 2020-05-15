f1415bcd49d17a4471fa908a2a13cb4ce1c2287a
Momo <seckndanane16@gmail.com>
2020-05-15 03:43:36 +0200

    troisième essai de l'exo 20

--- mo/codingGame/20_listHtml.php
+++ mo/codingGame/20_listHtml.php
@@ -4,19 +4,20 @@ function listHTML(string $titre, array $capital){
     if(is_null($titre) && empty($titre) || empty($capital))
     {
         return null;
-    }else{
+    }
         echo "<h3>$titre<h3/>";
 
-        echo "<ul>";
+        return "<ul>";
             foreach($capital as $capitale)
             {
-                echo "<li>".$capitale."<li/>";
+                return "<li>".$capitale."<li/>";
             }
-        echo "</ul>";
-    }
-    return null;
+        return "</ul>";
+    
 }
 var_dump(listHTML($titre = "SENEGAL", $capital=["moi", "toi", "lui"]));
+
+https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
 
 
 /*
