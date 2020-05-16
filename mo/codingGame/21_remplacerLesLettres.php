d5cdf2844eca0580a6111d5abb3404dc769fa970
Momo <seckndanane16@gmail.com>
2020-05-16 04:32:16 +0200

    etude des fonctions substr_replace ET str_replace

---
+++ mo/codingGame/21_remplacerLesLettres.php
@@ -0,0 +1,35 @@
+<?php
+// Créer une fonction from scratch qui s'appelle remplacerLesLettres(). Elle prendra un argument de type string. Elle devra retourner cette même string mais en remplacant les e par des 3, les i par des 1 et les o par des 0 Exemple :
+
+$vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
+$onlyconsonants = str_replace($vowels, "", "Hello World of PHP");
+echo $onlyconsonants;
+echo "<hr>";
+
+$phrase  = "You should eat fruits, vegetables, and fiber every day.";
+$healthy = array("fruits", "vegetables", "fiber");
+$yummy   = array("pizza", "beer", "ice cream");
+
+$newphrase = str_replace($healthy, $yummy, $phrase);
+echo $newphrase;
+
+echo "<hr>";
+
+$search  = array('A', 'B', 'C', 'D', 'E');
+$replace = array('B', 'C', 'D', 'E', 'F');
+$subject = 'A';
+echo str_replace($search, $replace, $subject);
+
+echo "<hr>";
+
+$var = 'ABCDEFGH:/MNRPQR/';
+echo "Original : $var<hr />\n";
+
+// Remplace toute la chaîne $var par 'bob'.
+echo substr_replace($var, 'bob', 0) . "<br />\n";
+echo substr_replace($var, 'bob', 0, strlen($var)) . "<br />\n";
+
+echo "<hr>";
+
+echo substr_replace($var, 'bob', 10, -1) . "<br />\n";
+echo substr_replace($var, 'bob', -7, -1) . "<br />\n";
