0906daa3bf16e7345259368422f64f58b2dfc22d
Momo <seckndanane16@gmail.com>
2020-05-17 04:02:38 +0200

    premiere solution de l'exo 21

--- mo/codingGame/21_remplacerLesLettres.php
+++ mo/codingGame/21_remplacerLesLettres.php
@@ -1,7 +1,23 @@
 <?php
 // Créer une fonction from scratch qui s'appelle remplacerLesLettres(). Elle prendra un argument de type string. Elle devra retourner cette même string mais en remplacant les e par des 3, les i par des 1 et les o par des 0 Exemple :
 
-$vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
+
+function remplacerLesLettres($search, $trouve, $phrase){
+    
+    return str_replace($search, $trouve, $phrase);
+
+}
+$ph = "Bonjour les amis";
+$se = array("e", "i", "o");
+$tr = array("3", "1", "0");
+
+print_r (remplacerLesLettres($se, $tr, $ph));
+
+
+
+
+
+/*$vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
 $onlyconsonants = str_replace($vowels, "", "Hello World of PHP");
 echo $onlyconsonants;
 echo "<hr>";
@@ -32,4 +48,4 @@ echo substr_replace($var, 'bob', 0, strlen($var)) . "<br />\n";
 echo "<hr>";
 
 echo substr_replace($var, 'bob', 10, -1) . "<br />\n";
-echo substr_replace($var, 'bob', -7, -1) . "<br />\n";
+echo substr_replace($var, 'bob', -7, -1) . "<br />\n";*/
