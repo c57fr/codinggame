ed104fb87c2459af8fd0a7bb718a80e457805a2b
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-17 17:53:35 +0200

    Soluce 21

--- mo/codingGame/21_remplacerLesLettres.php
+++ mo/codingGame/21_remplacerLesLettres.php
@@ -1,21 +1,22 @@
 <?php
-// Créer une fonction from scratch qui s'appelle remplacerLesLettres(). Elle prendra un argument de type string. Elle devra retourner cette même string mais en remplacant les e par des 3, les i par des 1 et les o par des 0 Exemple :
 
-
-function remplacerLesLettres($search, $trouve, $phrase){
-    
-    return strtr($phrase, array_combine($search, $trouve));
-
+function remplacerLesLettres($search, $trouve, $phrase)
+{
+  return strtr($phrase, array_combine($search, $trouve));
 }
-$phrase = "L3s c0urs d3 pr0grammat10n W3b s0nt tr0ps c00ls";
-$search = array("e", "i", "o");
-$trouve = array("3", "1", "0");
 
-print_r (remplacerLesLettres($search, $trouve, $phrase));
+function remplacerLesLettresLi($str)
+{
+  return str_ireplace('o','0', str_ireplace('e','3', str_ireplace('i','1', $str)));
+}
 
+$phrase = 'L3s c0urs d3 pr0grammat10n W3b s0nt tr0ps c00ls';
+$search = ['e', 'i', 'o'];
+$trouve = ['3', '1', '0'];
 
+print_r(remplacerLesLettres($search, $trouve, $phrase));
 
-
+// Créer une fonction from scratch qui s'appelle remplacerLesLettres(). Elle prendra un argument de type string. Elle devra retourner cette même string mais en remplacant les e par des 3, les i par des 1 et les o par des 0 Exemple :
 
 /*$vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
 $onlyconsonants = str_replace($vowels, "", "Hello World of PHP");
