3aef11f612c0ccd552cb0585501b781b3b0da5ca
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-18 19:45:28 +0200

    Corr

--- mo/codingGame/22_quelleAnnee.php
+++ mo/codingGame/22_quelleAnnee.php
@@ -1,9 +1,11 @@
 <?php
-//Créer une fonction from scratch qui s'appelle quelleAnnee(). Elle devra retourner un integer representant l'année actuelle.
 
-function quelleAnnee($annee){
-    return date("Y");
+function quelleAnnee()
+{
+  return date('Y');
 }
-echo "Nous sommes en ".quelleAnnee("Y");
+echo 'Nous sommes en '.quelleAnnee('Y');
+
+// Créer une fonction from scratch qui s'appelle quelleAnnee(). Elle devra retourner un integer representant l'année actuelle.
 
 //https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
