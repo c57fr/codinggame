303e72eebc06cd1038587a0a31dde7aae9d7f051
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-18 20:17:10 +0200

    Optimisation 24

--- mo/codingGame/24_fonctionMagique.php
+++ mo/codingGame/24_fonctionMagique.php
@@ -1,10 +1,11 @@
 <?php
-//Créer une fonction from scratch qui s'appelle fonctionMagique(). Il faut intégrer la librairie ./libraryToInclude.php
 
-//Il vous suffira pour completer cette exercice de retourner simplement la valeur produite par la fonction fonctionDeMaLibrairie() qui se trouve justement dans la librairie incluse.
 
-include("./libraryToInclude.php");
-
+include './libraryToInclude.php';
 function fonctionMagique(){
     return fonctionDeMaLibrairie();
 }
+
+// Créer une fonction from scratch qui s'appelle fonctionMagique(). Il faut intégrer la librairie ./libraryToInclude.php
+
+// Il vous suffira pour completer cette exercice de retourner simplement la valeur produite par la fonction fonctionDeMaLibrairie() qui se trouve justement dans la librairie incluse.
