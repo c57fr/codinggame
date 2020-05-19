a1a4fef5026b516b94c739dcf7a29dacdef4aba9
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-19 20:49:03 +0200

    Garder énoncé

--- mo/codingGame/25_getUtilisateursAutorises.php
+++ mo/codingGame/25_getUtilisateursAutorises.php
@@ -4,12 +4,10 @@ if (!function_exists('vdli')) {
   include '../../dev/vdli.php';
 } // vdli()
 
-/**
- * Juste pour simuler cette function du test dans codingame.
- */
+
 function getAllUtilisateurs()
 {
-  $allUsers =  [ // age, email, blocked
+  $allUsers = [ // age, email, blocked
     [9,   'Pier@ici.com',      false],
     [19,  'Pol@la.com',        true],
     [99,  'Jack@ailleurs.com', false],
@@ -28,27 +26,35 @@ function getAllUtilisateurs()
   return $users;
 }
 
-
-
 function getUtilisateursAutorises(): array
 {
   $users     = getAllUtilisateurs();
   $autorised = [];
 
   foreach ($users as $user) {
-    if ($user->age>17 && !empty($user->email && !$user->blocked)) 
-
-    $autorised[] = $user;
-
+    if ($user->age > 17 && !empty($user->email && !$user->blocked)) {
+      $autorised[] = $user;
+    }
   }
 
   return $autorised;
 }
 
-  /*
-           return (unset) ($user < 18) OR filter_var($email, !FILTER_VALIDATE_EMAIL);
-           // NB: On ne demande pas de tester la validité du email, juste si il existe ou pas... ;-)
-   */
-
 vdli(getUtilisateursAutorises());
 
+//https://www.codingame.com/playgrounds/32339/exercices-de-php-pour-debutant
+
+/*
+Créer une fonction from scratch qui s'appelle getUtilisateursAutorises(). Il faut intégrer la librairie ./libraryToInclude.php
+
+La fonction retournera un array d'utilisateur autorisés.
+
+Il faut d'abord récupérer la liste de tous les utilisateurs. Cette liste se trouvera en appellant la fonction getAllUtilisateurs()
+
+Une fois tous les utilisateur récuperés, faire les opérations suivantes :
+
+Supprimer les utilisateurs bloqués.
+Supprimer les utilisateurs qui n'ont pas d'adresse email
+Supprimer les utilisateurs qui ont moins de 18 ans.
+Enfin retourner la nouvelle liste propre.
+*/
