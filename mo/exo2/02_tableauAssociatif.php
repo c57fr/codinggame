ae7dee299abb1d89f0aad5fe9c29d07c8d022be6
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-20 21:19:22 +0200

    Opti 02

--- mo/exo2/02_tableauAssociatif.php
+++ mo/exo2/02_tableauAssociatif.php
@@ -6,27 +6,23 @@ if (!function_exists('vdli')) {
 } // vdli()
 
 $tab = [
-    'Momo'=> 34,
-    'Lio' => 12,
-    'Aziz' => 24,
-    'Ablaye' => 4
+  'Momo'   => 34,
+  'Lio'    => 12,
+  'Aziz'   => 24,
+  'Ablaye' => 4,
 ];
 
-foreach($tab as $key => $valeur){
-    echo $key. " : " .$valeur ."<br/>";
+foreach ($tab as $key => $valeur) {
+  echo $key.' : '.$valeur.'<br/>';
 }
 vdli($tab);
 
+// Sol GC7
+foreach($tab as $k=>$v){
+  echo $k.':'.$v."\n";
+}
+vdli($tab);
 
+// Il faut afficher tous les éléments du tableau associatif $tab sous le format cle:valeur\n. Le nombre d'éléments du tableau n'est pas connu à priori.
 
-
-
-
-
-
-
-
-/*
-Il faut afficher tous les éléments du tableau associatif $tab sous le format cle:valeur\n. Le nombre d'éléments du tableau n'est pas connu à priori.
-*/
-?>
+// https://www.codingame.com/playgrounds/41820/exercice-php---base/exercice-2
