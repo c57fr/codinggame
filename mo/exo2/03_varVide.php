6bdf43d7b2d482f7c1459bad2f70d4dae7114f13
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-20 21:20:39 +0200

    Opti 03

--- mo/exo2/03_varVide.php
+++ mo/exo2/03_varVide.php
@@ -1,17 +1,19 @@
 <?php
+
 ob_implicit_flush(); // Pour actu xdebug ds chrome
 if (!function_exists('vdli')) {
   include '../../dev/vdli.php';
 } // vdli()
 
-
 $var;
-    if(empty($var))
-    {
-        echo $var = 10;
-    }else{
-        echo $var;
+    if (empty($var)) {
+      echo $var = 10;
+    } else {
+      echo $var;
     }
+
+// Sol GC7
+$var = (empty($var)) ? 10 : $var;
 
 //https://www.codingame.com/playgrounds/41820/exercice-php---base/exercice-3
 
