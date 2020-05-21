61524d980d85cb7c6528754f15e2f049e3483495
Momo <seckndanane16@gmail.com>
2020-05-21 21:13:04 +0200

    serie 2 : exo 5 (premier essai)

---
+++ mo/exo2/05_cleExiste.php
@@ -0,0 +1,44 @@
+<?php
+
+ob_implicit_flush(); // Pour actu xdebug ds chrome
+if (!function_exists('vdli')) {
+  include '../../dev/vdli.php';
+}
+
+$search_array = array('premier' => 1, 'second' => 4);
+
+/*foreach($search_array as $ks => $v)(
+  vdli($v)
+);*/
+
+if(key($search_array)){
+  echo true;
+}else{
+  echo false;
+}
+/*
+$res = @array_key_exists("$key", $search_array);
+if($res){
+  echo true;
+}else{
+  echo false;
+}*/
+
+//$res = array_key_exists($key, $array);
+//vdli($res);
+/*
+echo '<hr/>';
+
+$search_array = array('premier' => 1, 'second' => 4);
+if (array_key_exists('premier', $search_array)) {
+    echo "L'élément 'premier' existe dans le tableau";
+}*/
+
+
+
+
+
+// https://www.codingame.com/playgrounds/41820/exercice-php---base/exercice-5
+
+// Il faut vérifier qu'une clé existe dans le tableau associatif $tableau. Si la clé $cle existe la variable $res sera TRUE sinon FALSE
+?>
