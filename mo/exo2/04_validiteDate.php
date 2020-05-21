35563bc5b1d684a13488d5b5ee2e46e84a634cbd
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-21 12:42:43 +0200

    Sol 04

--- mo/exo2/04_validiteDate.php
+++ mo/exo2/04_validiteDate.php
@@ -5,28 +5,25 @@ if (!function_exists('vdli')) {
   include '../../dev/vdli.php';
 } // vdli()
 
-$jour = date('d');
-$mois = date('m');
+$jour  = date('d');
+$mois  = date('m');
 $annee = date('Y');
 //echo $jour.'/'.$mois.'/'.$annee;
 $res = date($jour.'/'.$mois.'/'.$annee);
-if($res = date('d/m/Y')){
-    echo true;
-}else {
-    echo false;
+if ($res = date('d/m/Y')) {
+  echo true;
+} else {
+  echo false;
 }
 vdli($res);
 echo '<hr>';
-if($res = date('d/m/Y') ? true : false)
-vdli($res);
+if ($res = date('d/m/Y') ? true : false) {
+  vdli($res);
+}
 
-
-
-
-
-
+// Sol GC7
+$res = checkdate($mois, $jour, $annee);
 
 //Il faut tester la validité d'une date fournie grâce aux variables $jour, $mois et $annee. Si la date est valide la variable $res sera TRUE sinon FALSE.
-
 
 // https://www.codingame.com/playgrounds/41820/exercice-php---base/exercice-4
