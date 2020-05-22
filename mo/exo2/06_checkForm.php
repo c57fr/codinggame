1b864ad78a67dfe6e7567f8c74b7078c32818a33
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-23 00:41:54 +0200

    Corr test

--- mo/exo2/06_checkForm.php
+++ mo/exo2/06_checkForm.php
@@ -6,31 +6,31 @@ if (!function_exists('vdli')) {
 }
 
 $datas = [
-    'valide' => true,
-    'nom' => 'SECK',
-    'prenom' => 'Momo',
-    'CP' => "[1000, 9999]",
-    'naissance' => 29/07/1986,
-    'banque' => 'BE15 1234 5678 9012'
+  'valide'    => true,
+  'nom'       => 'SECK',
+  'prenom'    => 'Momo',
+  'CP'        => '[1000, 9999]',
+  'naissance' => 29 / 07 / 1986,
+  'banque'    => 'BE15 1234 5678 9012',
 ];
 //vdli($data);
 
-function check_form($data){
-    //$datas = [];
-   
-    foreach($data as $k => $dat)
-    {
-        //echo $k. " : " .$dat."<br/>";
-        $valide = [];
-        $message = [];
-        if(preg_match('#^[A-Za-z -]*$#', in_array($k=='nom', $data)))
-        {
-            echo $dat;
-        }else{
-            echo 'no';
-        }
-    }
+function check_form($data)
+{
+  //$datas = [];
 
+  foreach ($data as $k => $dat) {
+    //echo $k. " : " .$dat."<br/>";
+    $valide  = [];
+    $message = [];
+    if ('nom' === $k) {
+      if (preg_match('#^[A-Za-z -]*$#', in_array($k, $data, true))) {
+        echo $dat;
+      } else {
+        echo 'no';
+      }
+    }
+  }
 }
 
 vdli($datas);
@@ -40,7 +40,7 @@ vdli(check_form($datas));
 /*
 foreach ($datas as $key => $valeur) {
     echo $key.' : '.$valeur.'<br/>';
-    
+
 }
 if(array_key_exists($key, $datas))
 {
@@ -49,40 +49,12 @@ if(array_key_exists($key, $datas))
     return $datas;
 }*/
 
-
   //if(array_key_exists($datas['nom']))
 
 /*function donnees($key, $value){
     echo "$key : $value";
 }*/
 
-
-
-
 //vdli(check_form($datas));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // https://www.codingame.com/playgrounds/41820/exercice-php---base/exercice-6
