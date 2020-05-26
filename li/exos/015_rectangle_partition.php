91e36dfe0201be95d965c9dd6965c9a757da9d3b
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-26 13:45:15 +0200

    Sol + efficace 015

--- li/exos/015_rectangle_partition.php
+++ li/exos/015_rectangle_partition.php
@@ -11,34 +11,6 @@ ob_implicit_flush(); // Pour actu xd.ebug ds chrome
 // Reconstitution des données du test
 // t 1
 
-$t  = ['orange', 'citron', 'banane', 'pomme'];
-$n  = count($t);
-$ct = []; // Tableau qui va contenir les rcombinaisons de tags
-
-function listeCombi($debut, $tags, $profondeur)
-{
-  global $ct;
-
-  if (0 === $profondeur) {
-    array_push($ct, $debut);
-
-    return;
-  }
-  $n = count($tags);
-  for ($i = 0; $i < $n; ++$i) {
-    listeCombi($debut.$tags[$i].',', array_slice($tags, $i + 1), $profondeur - 1);
-  }
-}
-
-for ($i = 1; $i <= $n; ++$i) {
-  listeCombi(',', $t, $i);
-}
-
-// echo "<pre>";
-// print_r($ct);
-// echo"</pre>";
-
-// exit;
 $w      = 10;
 $h      = 5;
 $countX = 2;
@@ -47,43 +19,67 @@ $countY = 1;
 $xs = [2, 5];
 array_unshift($xs, 0);
 array_push($xs, $w);
+// sort($xs);
 
 $ys = [3];
 array_unshift($ys, 0);
 array_push($ys, $h);
+// sort($ys);
 
 // vdli($xs);
 // vdli($ys);
+/*
+Recycling the ample:
+
+  0___2______5__________10
+  |   |      |          |
+  |   |      |          |
+ 3|___|______|__________|
+  |   |      |          |
+ 5|___|______|__________|
+Possible distances:
+
+Horizontal: 2 3 5 5 8 10
+            | | |/
+Vertical:   2 3 5
+=> 4 possible squares.
+*/
 
 // echo '<hr>';
 function getCombs($arr)
 {
+  // vdli($arr);
+  $combs=[];
   foreach ($arr as $k => $v) {
     // echo $v.' ';
     foreach ($arr as $k2 => $v2) {
-      if ($v !== $v2) {
-        $cs = [$v, $v2];
-        sort($cs);
-        $combs[$cs[0].$cs[1]] = $cs[1] - $cs[0];
+      if ($v > $v2) {
+        // $cs = [$v, $v2];
+        // sort($cs);
+        // $combs[$cs[0].$cs[1]] = $v - $v2;
+        $combs[] = $v - $v2;
       }
     }
   }
-
+  // vdli($combs);
   // return array_unique($combs);
   // $combs=sort($combs);
   return $combs;
 }
 $axs = getCombs($xs);
 $ays = getCombs($ys);
-vdli($axs);
-vdli($ays);
+// sort($axs);
+// sort($ays);
+// vdli($axs);
+// vdli($ays);
 
-$q=0;
+// exit;
+$q = 0;
 foreach ($ays as $k => $v) {
   foreach ($axs as $k2 => $v2) {
     if ($v === $v2) {
       // echo $v.' x '.$v2.'<br>';
-      $q++;
+      ++$q;
     }
   }
 }
