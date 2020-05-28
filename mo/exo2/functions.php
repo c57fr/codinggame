da5414c09eb43f37ec000327275bc70a0dd69017
Momo <seckndanane16@gmail.com>
2020-05-28 21:35:17 +0200

    exo 2 de l'introduction aux fonctions

--- mo/exo2/functions.php
+++ mo/exo2/functions.php
@@ -1,9 +1,56 @@
 <?php
 
-// dire bonjour
-function bonjour(){
-    echo "Bonjour à tous<br/>";
+$prenom = 'Pierre';
+$x = 4;
+$y = 5;
+
+function bonjour($p){
+    echo "Bonjour " .$p. "<br/>";
 }
-bonjour();
-bonjour();
-bonjour();
+
+function addition($p1, $p2){
+    echo $p1 . " + " .$p2 . " = " . ($p1 + $p2) . "<br>";
+}
+bonjour($prenom);
+bonjour("Mathilde");
+addition($x, $y);
+addition(1,1);
+echo '<hr>';
+
+$nom = "SECK";
+$prenom = "Momo";
+
+$c = 40;
+$s =5;
+
+function direBonjour($b){
+    echo "Bonjour " .$b. "<br/>"; 
+}
+
+function multiplication($a, $b){
+    echo $a . " * " . $b . " = ". ($a * $b) . "<br/>";
+}
+
+direBonjour($nom);
+direBonjour($prenom);
+direBonjour("Lio");
+multiplication($c,$s);
+
+echo "<hr>";
+
+$prenom = "Aziz";
+$nom = "SECK";
+$n = 33;
+$m = 5;
+
+function priere($p){
+    echo "Que la paix soit avec toi " .$p."<br/>";
+}
+
+function division($d, $o){
+    echo $d. " / " .$o. " = " . ($d / $o) . "<br/>";
+}
+priere($prenom);
+priere($nom);
+priere("Abdoulaye SECK");
+division($n, $m);
