3b62595f15e91f432295eeeccbf50a4f36296066
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-28 20:40:39 +0200

    Aff 1 de matrice

--- li/exos/017_lumen.php
+++ li/exos/017_lumen.php
@@ -14,13 +14,25 @@ $N     = 5;
 $L     = 3;
 $LINES = [
   'X X X X X',
-  'X C X X X',
+  'X X X C X',
   'X X X X X',
   'X X X X X',
   'X X X X X',
 ];
 
 vdli($LINES);
+
+for ($i = 0; $i < $N; ++$i) {
+  $m[$i] = explode(' ', $LINES[$i]);
+}
+
+for ($i=0; $i < $N; $i++) { 
+  for ($j=0; $j < $N; $j++) { 
+    echo $m[$i][$j];
+  }
+  echo '<br>';
+}
+
 
 //##################################################################
 /*
