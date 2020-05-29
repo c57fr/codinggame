1465409012bb241e0c8517a833acd7dc82180257
Momo <seckndanane16@gmail.com>
2020-05-29 03:26:45 +0200

    test avec typage stricte

--- mo/exo2/functions.php
+++ mo/exo2/functions.php
@@ -1,16 +1,12 @@
 <?php
 
-function test($a, $b){
-    echo $a. " + " .$b. " = " .($a + $b)."<br/>";
-}
+declare(strict_types= 1);
+
 
 function addition(float $a, float $b){
     echo $a. " + " .$b. " = " .($a + $b). "<br/>";
 }
 
-test(3, 4);
-test(3, '4Pierre');
-test(3, 'Pierre');
 addition(3, 4);
 addition(3, 4.5);
 addition(3.5, 4.2);
