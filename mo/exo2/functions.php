723442d6d1a07424431b5d92ac537e057380e8a1
Momo <seckndanane16@gmail.com>
2020-05-29 03:23:58 +0200

    test sans typage stricte

--- mo/exo2/functions.php
+++ mo/exo2/functions.php
@@ -1,9 +1,18 @@
 <?php
 
-function bonjour(...$prenom){
-    foreach($prenom as $p)
-    {
-        echo "Bonjour " .$p. "<br/>";
-    }
+function test($a, $b){
+    echo $a. " + " .$b. " = " .($a + $b)."<br/>";
 }
-bonjour('Pierre', 'Momo', 'Lio');
+
+function addition(float $a, float $b){
+    echo $a. " + " .$b. " = " .($a + $b). "<br/>";
+}
+
+test(3, 4);
+test(3, '4Pierre');
+test(3, 'Pierre');
+addition(3, 4);
+addition(3, 4.5);
+addition(3.5, 4.2);
+addition(3, '4Pierre');
+addition(3, 'Pierre');
