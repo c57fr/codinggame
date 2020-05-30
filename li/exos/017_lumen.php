173655541c7846d305600f11e30331f63f737411
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-30 12:37:24 +0200

    Sol 01 - 017

--- li/exos/017_lumen.php
+++ li/exos/017_lumen.php
@@ -16,18 +16,22 @@ ob_implicit_flush(); // Pour actu xd.ebug ds chrome
 $N     = 5;
 $L     = 3;
 $LINES = [
-  'X X X X X',
-  'X C X X X',
-  'X X X X X',
+  'C X X X C',
   'X X X X X',
   'X X X X X',
+  'X X X X X',
+  'C X X X C',
 ];
 
+
+
+// exit;
+// Sol 01
 // Recherche des candles
 for ($i = 0; $i < $N; ++$i) {
   $m[$i] = explode(' ', str_replace('X', 0, $LINES[$i]));
 }
-// affM($m);
+affM($m);
 
 // $cs=[];
 for ($i = 0; $i < $N; ++$i) {
@@ -99,11 +103,12 @@ function recursiveLights($m, $i, $j, $lightPower)
 //   affM($m);
 
 foreach ($cs as $k => $v) {
-//   echo $k.' ('.$v[0].','.$v[1].')';
+  echo $k.' ('.$v[0].','.$v[1].' - '.$L.')<br>';
   recursiveLights($m, $v[0], $v[1], $L);
 }
+echo '<br>';
 
-//   affM($m);
+  affM($m);
   echo get0($m);
 
 //##################################################################
