a2bf75f79c573a42c2d976b94784c96fa699fa44
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-30 19:37:16 +0200

    2Fix T5 - 017

--- li/exos/018_graffiti.php
+++ li/exos/018_graffiti.php
@@ -10,17 +10,79 @@ ob_implicit_flush(); // Pour actu xd.ebug ds chrome
 
 // Reconstitution test 1
 
-$L  = 10;
-$N  = 2;
-$rs = [[1, 4], [5, 6]];
+// $L  = 10;
+// $N  = 2;
+// $rs = [[1, 4], [5, 6]];
+
+// Reconstitution test 2
+// $L  = 12;
+// $N  = 5;
+// $rs = [
+//   [6, 10],
+//   [0, 4],
+//   [7, 8],
+//   [3, 7],
+//   [8, 12],
+// ];
+
+// Test 5
+$L  = 100;
+$N  = 10;
+$rs = [
+  0 => [
+    0 => 2,
+    1 => 3,
+  ],
+  1 => [
+    0 => 3,
+    1 => 20,
+  ],
+  2 => [
+    0 => 20,
+    1 => 30,
+  ],
+  3 => [
+    0 => 30,
+    1 => 40,
+  ],
+  4 => [
+    0 => 41,
+    1 => 60,
+  ],
+  5 => [
+    0 => 60,
+    1 => 70,
+  ],
+  6 => [
+    0 => 70,
+    1 => 88,
+  ],
+  7 => [
+    0 => 88,
+    1 => 99,
+  ],
+  8 => [
+    0 => 99,
+    1 => 100,
+  ],
+  9 => [
+    0 => 40,
+    1 => 41,
+  ],
+];
+
+// vdli($rs);
+sort($rs);
 // vdli($rs);
 
-$m = array_merge([0], range(1, 9));
-// vdli($m);
+$m = array_merge([0], range(1, $L - 1));
+vdli($m);
 // $n = range(1, 3);
 foreach ($rs as $k => $v) {
   // vdli(range($v[0], $v[1] - 1));
-  $m = array_diff($m, range($v[0], $v[1] - 1) );
+  $m = array_diff($m, range($v[0], $v[1] - 1));
+  echo 'Retrait de '.$v[0].' '.($v[1] - 1);
+  vdli($m);
 }
 // $o = range(5, 5);
 // vdli($n);
@@ -28,7 +90,7 @@ foreach ($rs as $k => $v) {
 
 // $rs = array_merge($n, $o);
 // $rs = array_diff($m, $n);
-// vdli($m);
+vdli($m);
 $rs = array_reverse($m);
 // vdli($rs);
 
@@ -49,27 +111,30 @@ $sols = [];
 
 // $r = array_map(function($v){return $v;}, $r);
 
-$nfence = 1;
-for ($i = 0; $i < $n - 1; ++$i) {
-  // echo $rs[$i];
-  if ($rs[$i] - 1 === $rs[$i + 1]) {
-    unset($rs[$i]);
-    ++$nfence;
-  } else {
-    $sols[$rs[$i]] = $nfence;
-    $nfence        = 1;
+if (count($m)) {
+  $nfence = 1;
+  for ($i = 0; $i < $n - 1; ++$i) {
+    // echo $rs[$i];
+    if ($rs[$i] - 1 === $rs[$i + 1]) {
+      unset($rs[$i]);
+      ++$nfence;
+    } else {
+      $sols[$rs[$i]] = $nfence;
+      $nfence        = 1;
+    }
   }
+  $sols[$rs[$i]] = 1;
+  ksort($sols);
+  foreach ($sols as $k => $v) {
+    echo $k.' '.($k + $v).'<br>';
+    // code...
+  }
+} else {
+  echo 'All painted'."\n";
 }
-$sols[$rs[$i]] = 1;
 
 // vdli($rs);
 // vdli($sols);
-
-ksort($sols);
-foreach ($sols as $k => $v) {
-  echo $k.' '.($k + $v).'<br>';
-  // code...
-}
 
 // exit;
 
