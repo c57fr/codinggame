e6dca50632e16594cf2068bc048df93d4a98a702
Momo <seckndanane16@gmail.com>
2020-05-30 07:41:26 +0200

    Accéder à une variable de portée globale depuis un espace local

--- mo/exo2/functions.php
+++ mo/exo2/functions.php
@@ -1,27 +1,11 @@
 <?php
 $x = 10;
 
-function portee1(){
-    echo "La valeur globale de $x globale est : " .$x. "<br>";
+function portee(){
+    global $x;
+    echo "La valeur globale de $x est : " . "<br>";
+    $x = $x + 5;
 }
 
-function portee2(){
-    $x = 5;
-    echo "La valeur globale de $x locale est : " .$x. "<br>";
-}
-
-function portee3(){
-    $y = 0;
-    $y++;
-    echo '$y contient la valeur : ' .$y. '<br>';
-}
-
-function portee4(){
-    $z = 1;
-}
-
-portee1();
-portee2();
-portee3();
-portee3();
-portee4();
+portee();
+echo '$x contient maintenant : ' .$x. "<br>";
