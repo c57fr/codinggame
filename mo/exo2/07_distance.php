b0d2597be8b8488c249b85f4e01b8f0d61670120
GrCOTE7 <GrCOTE7@GMail.com>
2020-05-31 10:55:11 +0200

    Pour tester - 07

--- mo/exo2/07_distance.php
+++ mo/exo2/07_distance.php
@@ -5,33 +5,14 @@ if (!function_exists('vdli')) {
   include '../../dev/vdli.php';
 }
 
-function distance($x1, $x2, $y1, $y2){
-   $res = sqrt($x2 - $x1)($x2 + $x1) + sqrt($y2 - $y1)($y2 + $y1);
-    return count($res);
+function distance($x1, $x2, $y1, $y2)
+{
+  $res = sqrt($x2 - $x1)($x2 + $x1) + sqrt($y2 - $y1)($y2 + $y1);
+
+  return count($res);
 }
 
+vdli(distance(0, 1, 0, 1));
 
-/*function distance($x1, $x2, $y1, $y2){
-    $res = sqrt($x2 - $x1)2 + sqrt($y2 - $y1)2;
-     return ($res);
- }*/
+// https://www.codingame.com/playgrounds/41820/exercice-php---base/exercice-7
 
-//vdli(distance($a - $b));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//https://www.codingame.com/playgrounds/41820/exercice-php---base/exercice-7
