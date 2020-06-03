c228f1cd91737e5da5e72d4c06ffee29cc750afd
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-03 14:24:54 +0200

    Sol 1 - 018

--- li/exos/018_graffiti.php
+++ li/exos/018_graffiti.php
@@ -10,273 +10,104 @@ ob_implicit_flush(); // Pour actu x.debug ds chrome
 
 $L = 10;
 
-// $ss = [[5, 7], [9, 10], [1, 3]]; // 3 ss juxtaposés
+$ss = [[5, 7], [9, 10], [1, 3]]; // 3 ss juxtaposés
 // $ss = [[2, 5], [4, 8]];
-$ss = [[2, 4], [6, 8], [3, 10]];
+// $ss = [[2, 4], [6, 8], [3, 10]];
+// $ss = [[4, 6], [6, 8], [0, 2], [3, 9]];
 
-$N = count($ss);
-// affMM($ss);
 // Reconstitution test 1
-
-// $L   = 10;
-// $N   = 2;
-// $ps = [[1, 4], [5, 6]];
-
-// $m[0] = [0, 10];
-
-//1 4
-
-// exit;
-
-// vdli($pts);
-
-// Reconstitution test 6
-
-/*
-$L  = 2e9;
-$N  = 2;
-$ss = [
-  [
-    0 => 6,
-    1 => 10,
-  ],
-  [
-    0 => 2,
-    1 => 4,
-  ],
-  [
-    0 => 7,
-    1 => 8,
-  ],
-  [
-    0 => 3,
-    1 => 7,
-  ],
-  [
-    0 => 8,
-    1 => 12,
-  ],
-];
-*/
-
-// foreach ($se as $v) {
-//   echo $v[0].' '.$v[1].'<br>';
-// }
-$ms[0] = [0, $L];
-// vdli($m);
-// 1,4
-// $m[0] = $se[0][0];
-// $m[$se[0][1]] = 10;
-
-// $m = array_chunk($m, 1);
-
-// foreach ($pts as $k => $v) {
-//   echo 'Cas de ['.$pts[$k][0].', '.$pts[$k][1].'] :<br>';
-
-// 1 4
-// 5 6
-// unset($ss[2]);
-// affMM($ss);
-
-// vdli(affMM($ms));
-
-// array_shift($ss); // SegmentS
-
-$sas = [$ss[0]]; // Segments AgglutinéS
-affMM($ss);
-
-for ($i = 1; $i < $N; ++$i) { // Chaque SegmentS tour à tour
-  echo '<hr>';
-  $tt = 0;
-  foreach ($sas as $k => $s) { // pour chaque Morceau déjà agglutiné
-    while (!$tt) {
-      echo '<br>Cas [ '.$ss[$i][0].', '.$ss[$i][1].' ] // [ '.$s[0].', '.$s[1].' ] :<br>';
-
-      if ($ss[$i][0] > $s[1] || $ss[$i][1] < $s[0]) { // Juxtaposé
-        echo 'À côté du segment déjà placé '.$k.'<br>';
-        $sas[] = $ss[$i];
-        $tt    = 1;
-      } else { // Chevauche
-        echo 'Chevauche segment déjà placé '.$k.'<br>';
-        $sas[$k] = [min($s[0], $ss[$i][0]), max($s[1], $ss[$i][1])];
-        foreach ($sas as $m => $v) {
-          if ($m !== $k) {
-            if ($v[0] >= $sas[$k][0] && $v[1] <= $sas[$k][1]) {
-              echo 'oooo'.$m;
-            }
-            unset($sas[$m]);
-          }
-        }
-        $tt = 1;
-      }
-    }
-  }
-  // echo implode(' ', $sas[$k]).'<br><br>';
-  echo affMM($sas);
-}
-
-// sort($ps);
-affMM($sas);
-exit;
-
-foreach ($ds as $s) {
-  echo 'Segment '.$s[0].', '.$s[1].' :<br>';
-  // echo affM2x2($ss).' :<br>';
-
-  foreach ($ms as $i => $m) {
-    if ($m[0] <= $s[0] && $m[1] >= $s[1]) {
-      echo 'Insert dans : '.$m[0].' '.$m[1].' possible.';
-      if ($s[1] !== $m[1]) {
-        $ms[] = [$s[1], $m[1]];
-      }
-      $ms[$i] = [$m[0], $s[0]];
-      affMM($ms);
-    // vdli($m);
-    } elseif ($m[0] >= $s[0] && $m[1] <= $s[1]) {
-      $ecr = [];
-      echo 'Ecrasement du morceau '.$i.' : '.$m[0].', '.$m[1].'<br>';
-      $ecr[] = $i;
-      // unset($ms[$i]);
-
-      echo implode(', ', $ecr);
-      // vdli($ecr);
-      // affMM($ecr);
-    }
-  }
-}
-
-function insertD($se, $m)
-{
-  return $m;
-}
-
-// foreach ($ss as $k => $v) {
-//   if (!$k) {
-//     $m[]  = [$v[1], $L];
-//     $m[0] = [0, $v[0]];
-//   }
-//   // $m[]   = [$v[1], 10];
-//     // $m[$k] = [$m[$k][0], $v[0]];
-// }
-echo '<hr>';
-
-affMM($ms);
-
-  // echo $k. ' '.$v[0].'<br>';
-  // $m[$k+1][0] = $v[0];
-  // $m[$k+1][$v[1]] = $m[0][0];
-// }
-
-// echo 'Cas de ['.$se[1][0].', '.$se[1][1].'] :';
-
-// $m[4] = 5;
-// $m[6] = 10;
-
-// vdli($m);
-exit;
+$L  = 10;
+$ss = [[1, 4], [5, 6]];
 
 // Reconstitution test 2
 // $L  = 12;
-// $N  = 5;
-// $rs = [
-//   [6, 10],
-//   [0, 4],
-//   [7, 8],
-//   [3, 7],
-//   [8, 12],
-// ];
+// $ss = [[6, 10], [0, 4], [7, 8], [3, 7], [8, 12]];
 
 // Test 5
-// $L  = 100;
+// $L = 100;
 // $N  = 10;
-// $rs = [
-//   0 => [
-//     0 => 2,
-//     1 => 3,
-//   ],
-//   1 => [
-//     0 => 3,
-//     1 => 20,
-//   ],
-//   2 => [
-//     0 => 20,
-//     1 => 30,
-//   ],
-//   3 => [
-//     0 => 30,
-//     1 => 40,
-//   ],
-//   4 => [
-//     0 => 41,
-//     1 => 60,
-//   ],
-//   5 => [
-//     0 => 60,
-//     1 => 70,
-//   ],
-//   6 => [
-//     0 => 70,
-//     1 => 88,
-//   ],
-//   7 => [
-//     0 => 88,
-//     1 => 99,
-//   ],
-//   8 => [
-//     0 => 99,
-//     1 => 100,
-//   ],
-//   9 => [
-//     0 => 40,
-//     1 => 41,
-//   ],
-// ];
+// $ss = [[2, 3], [3, 20], [20, 30], [30, 40], [41, 60], [60, 70], [70, 88], [88, 99], [99, 100], [40, 41]];
 
-// vdli($rs);
-sort($rs);
-// vdli($rs);
+// Reconstitution test 6
+// $L  = 2e9;
+// $ss = [[6, 10], [2, 4], [7, 8], [3, 7], [8, 12]];
 
-$m = array_merge([0], range(1, $L - 1));
-// vdli($m);
+// Reconstitution test 10
+// $L  = 2e9;
+// $N  = 50;
+// $ss = [[1006862452, 1054756458], [678724219, 1778223615], [678053276, 1866811373], [634631439, 1605931752], [306180988, 1921164334], [217773907, 1732031918], [214843117, 1454854895], [82072778, 1036085256], [26474374, 1522207404], [732389, 891781], [680953, 848366], [655134, 760376], [154824, 717381], [115599, 443074], [104050, 297168], [22878, 794282], [8994, 9650], [8637, 9357], [7126, 8203], [4844, 8366], [3286, 8241], [2712, 4447], [2150, 8103], [1830, 7805], [1742, 9949], [1458, 1832], [442, 452], [261, 365], [260, 475], [239, 6154], [208, 369], [165, 239], [156, 371], [122, 4995], [119, 264], [114, 119], [81, 98], [68, 88], [52, 92], [44, 97], [31, 41], [30, 60], [27, 36], [24, 55], [17, 438], [12, 14], [5, 80], [5, 83], [4, 67], [0, 20]];
 
-foreach ($rs as $k => $v) {
-  for ($i = $v[0]; $i < $v[1]; ++$i) {
-    unset($m[$i]); // code...
+// Reconstitution test 12
+// $L  = 7;
+// $ss = [[1, 5], [3, 6], [0, 2]];
+
+sort($ss);
+// $allss = array_chunk($ss, 6);
+// $ss    = $allss[0];
+// exit;
+// affMM($ss);
+$N = count($ss);
+// exit;
+
+$decal = 1;
+for ($i = 1; $i < $N; ++$i) {
+  // if (33 === $i) {
+  //   echo 'ok';
+  // }
+  echo $i.':  '.implode(', ', $ss[$i]).'<br>';
+  if ($ss[$i][0] <= $ss[$i - $decal][1]) {
+    $ss[$i - $decal][1] = max($ss[$i - $decal][1], $ss[$i][1]);
+    unset($ss[$i]);
+    ++$decal;
+  } else {
+    if (1 !== $i) {
+      $ss[$i - $decal + 1] = $ss[$i];
+      unset($ss[$i]);
+    }
   }
 }
-sort($m);
-// vdli($m);
-
-$sols = [];
-
-if (count($m)) {
-  $nfence = 1;
-  foreach ($m as $k => $v) {
+affMM($ss);
+// exit;
+// $L  = 8;
+// $ss = [[1, 8]];
+if (0 === $ss[0][0] && $ss[0][1] === $L) {
+  echo 'All painted';
+} else {
+  // affMM($ss);
+  array_unshift($ss, [0, $ss[0][0]]);
+  // affMM($ss);
+  $sols[0] = $ss[0];
+  foreach ($ss as $k => $p) {
+    if ($k) {
+      $sols[$k] = [$ss[$k - 1][1], $p[0]];
+    }
   }
-  for ($i = count($m) - 1; $i >= 0; --$i) {
-    if ($m[$i] - 1 === @$m[$i - 1]) {
-      // echo $m[$i];
-      ++$nfence;
-      unset($m[$i]);
-    } else {
-      // vdli($m[$i]);
-      $sols[] = [$m[$i], $nfence];
-      $nfence = 1;
+  // echo 'Solutions:';
+  if ($ss[$k][1] !== $L) {
+    $sols[] = [$ss[$k][1], $L];
+  }
+  sort($sols);
+  foreach ($sols as $k => $v) {
+    if ($v[0] === $v[1]) {
+      unset($sols[$k]);
     }
   }
 
-  sort($sols);
-  // vdli($m);
-  // vdli($sols);
+  affMM($sols);
 
-  foreach ($sols as $k => $v) {
-    echo $v[0].' '.($v[0] + $v[1]).'<br>';
-  }
-} else {
-  echo 'All painted'."\n";
+  // foreach ($sols as $k => $v) {
+  //   if ($v[0] === $v[1]) {
+  //     unset($sols[$k]);
+
+  //     continue;
+  //   }
+  //   echo $v[0].', '.$v[1]."\n";
+  // }
 }
 
-// exit;
+// foreach ($sols as $k => $v) {
+//   echo $v[0].', '.$v[1]."\n";
+// }
 
 //##################################################################
 /*
