d292b27cc2346b54a9de2576c4730c599a799ad9
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-03 23:29:00 +0200

    Prepa 019 - Pikaptcha

---
+++ li/exos/019_pikaptcha.php
@@ -0,0 +1,23 @@
+Exo 019: DETECTIVE PIKAPTCHA EP2.
+<hr>
+<?php
+if (!function_exists('vdli')) {
+  include '../../dev/vdli.php';
+}
+ob_implicit_flush(); // Pour actu x.debug ds chrome
+
+// https://www.codingame.com/ide/puzzle/detective-pikaptcha-ep2
+
+
+// Reconstitution test 1
+
+//##################################################################
+/*
+
+Solution dans codding :
+
+<?php
+
+
+
+*/
