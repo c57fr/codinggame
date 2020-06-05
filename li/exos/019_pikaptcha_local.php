9d117331b67e10b972b3314c9e356559b95d9885
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-05 14:19:16 +0200

    Repère départ - 019

--- li/exos/019_pikaptcha_local.php
+++ li/exos/019_pikaptcha_local.php
@@ -20,11 +20,17 @@ ob_implicit_flush(); // Pour actu x.debug ds chrome
 
 $width=5;
 $height=3;
-$line='>000##0#0000#0#';
+$l='>000##0#0000#0#';
 $side='L';
+$sens[1]='>';
 
-echo 'ok21';
+preg_match('/[^0+|#+]/', $l, $p, PREG_OFFSET_CAPTURE);
 
+echo 'Pos départ ( '.$p[0][0]. ' ) est '.$p[0][1];
+
+
+
+// vdli($pos);
 //##################################################################
 /*
 
