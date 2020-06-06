77defb57e95ebef96885726c7d9e2cc2be686922
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-06 13:00:19 +0200

    T3 - 019

--- li/exos/019_pikaptcha_local.php
+++ li/exos/019_pikaptcha_local.php
@@ -10,34 +10,60 @@ ob_implicit_flush(); // Pour actu x.debug ds chrome
 
 // Étude : https://fr.wikipedia.org/wiki/Algorithme_de_parcours_en_profondeur
 
-// Reconstitution test 1
-
+// Reconstitution Test 1
 // 5 3
 // >000#
 // #0#00
 // 00#0#
 // L
-
 $width  = 5;
 $height = 3;
-$l      = '>000##0#0000#0#';
-// $l      = '000^##0#0000#0#';
+$LINE   = ['>000#', '#0#00', '00#0#'];
+
+// Reconstitution Test 3
+// 3 3
+// 0#0
+// #>#
+// 0#0
+// L
+$width  = 3;
+$height = 3;
+$LINE   = ['0#0', '#>#', '0#0'];
+
+$w   = $width + 2;
+$h   = $height + 2;
+$arr = array_fill(0, $width, '#');
+array_unshift($LINE, implode('', $arr));
+array_push($LINE, implode('', $arr));
+$l = '';
+foreach ($LINE as $ligne) {
+  $l .= '#'.$ligne.'#';
+}
+echo $l;
+
 $side = 'L';
 
-$s = ['^', '>', 'v', '<'];
-$d = [-$width, 1, $width, -1];
-vdli($s);
-vdli($d);
+$ss = ['^', '>', 'v', '<'];
+$ds = [-$w, 1, $w, -1];
+vdli($ss);
+vdli($ds);
 
 preg_match('/[^0+|#+]/', $l, $pos, PREG_OFFSET_CAPTURE);
-echo 'Pos départ ( '.$pos[0][0].' ) est '.$pos[0][1];
+echo 'Pos départ ( '.$pos[0][0].' ) est '.$pos[0][1].'<br>';
 // exit;
 $p = $pos[0][1];
 // Calcul du sens
-$s = $d[array_search($pos[0][0], $s, true)];
+$s = $ds[array_search($pos[0][0], $ss, true)];
 
-if ('#' !== $l[$p]) {
-  $p += $s;
+$i = 0;
+while ($i <= 10) {
+  echo $p.' ('.$ss[array_search($s, $ds, true)].') ';
+
+  if ('#' !== $l[$p + $s]) {
+    $p += $s;
+  }
+
+  ++$i;
 }
 
 vdli($s);
