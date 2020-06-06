0b3122a5b8525ffa314bf033866d25bf10144b82
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-06 22:37:49 +0200

    Nett

--- mo/exo2/08_exo8.php
+++ mo/exo2/08_exo8.php
@@ -6,48 +6,32 @@ if (!function_exists('vdli')) {
 }
 
 echo 'Voyons un tablo de 3 lignes de 5 cases...<hr>';
-// lio laisse moi le faire pour voir si j'ai compris
 
-// oki, continue le code ci-dessous alors...
-//c'est ca ?
-// oui, là, ça affiche 012
-// comment je dois faire pour afficher dans mon nav en local ?
-// Pull the commit ! (reset local here dans GK)
-// j'ai pas trouve le pull?
-// Le commit est: der change (et tu dois le voir dans GK automatiquement)
-
-
-for ($y=0; $y < 3; $y++) { 
-    echo $y;
+for ($y = 0; $y < 3; ++$y) {
+  for ($x = 0; $x < 5; ++$x) {
+    echo $y.'<br>';
+  }
 }
-
 
 exit;
 
+function init_table($n, $m, $min, $max)
+{
+  $tab = [];
 
-function init_table($n, $m, $min, $max){
+  for ($y = 0; $y < $m; ++$y) {
+    for ($x = 0; $x < $n; ++$x) {
+      $tab[$x][$y] = rand($min, $n); // la fonction range permet de ranger et rand c'est pour les valeurs aleatoires
+      $tab[$x][$y] = rand($max, $m);
 
-    $tab = [];
-
-    for($y = 0; $y < $m; ++$y)
-    {
-        for($x = 0; $x < $n; ++$x)
-        {
-            $tab[$x][$y] = rand($min,$n); // la fonction range permet de ranger et rand c'est pour les valeurs aleatoires
-            $tab[$x][$y] = rand($max,$m);
-            return ($n * $m);       
-        }
+      return $n * $m;
     }
+  }
 
-
-
-    /*if($n >= $min && $m <= $max)
-    {
-        echo  "${n}".rand($min, $n) *  "${m}".rand($max, $m);
-    }*/
-
-
-
+  /*if($n >= $min && $m <= $max)
+  {
+      echo  "${n}".rand($min, $n) *  "${m}".rand($max, $m);
+  }*/
 
     /*$tableau = [];      //range($n * $m <= $min && $n * $m >= $max);
     for($i = 0; $i <= $min; $i++)
@@ -62,20 +46,15 @@ function init_table($n, $m, $min, $max){
     return $tableau[] = $n * $m;*/
 }
 
-vdli(init_table(2,4,0,20));
+vdli(init_table(2, 4, 0, 20));
 
-echo "<br>";
+echo '<br>';
 
 // pour le deuxieme tableau
 $tab = ['negative', 'positive', 'nulles'];
-foreach($tab as $ta)
-{
-    echo $ta. "<br>";
-    
+foreach ($tab as $ta) {
+  echo $ta.'<br>';
 }
-
-
-
 
 //$re = [];
 /*
