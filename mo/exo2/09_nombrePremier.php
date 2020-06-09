b5bd168176c63bdbf3b4730036f3353bd2e431c6
Momo <seckndanane16@gmail.com>
2020-06-09 02:16:33 +0200

    exo 10 premier test

---
+++ mo/exo2/09_nombrePremier.php
@@ -0,0 +1,29 @@
+<?php
+
+ob_implicit_flush(); // Pour actu xdebug ds chrome
+if (!function_exists('vdli')) {
+  include '../../dev/vdli.php';
+}
+
+function premiers($n){
+    for($i = 2; $i < $n; $i++)
+    {
+        //echo $i."<br>";
+        if ($n % $i == 0) {
+            return FALSE;
+        }
+    }
+    return true;
+}
+
+echo '<br>Nombre premier de 0 à 100: ';
+for ($i = 3; $i < 100; $i++) {
+    if (premiers($i))
+    {
+      echo $i.' ';
+    }
+}
+
+
+// un nombre premier est un nombre divisible par 1 et par lui-meme. Il faut savoir que 1 n'est pas un nombre premier
+//https://www.codingame.com/playgrounds/41820/exercice-php---base/exercice-9
