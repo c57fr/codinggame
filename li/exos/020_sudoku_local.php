0fbb987cc14842e0c1631a996080e723dc81ab3f
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-15 01:25:33 +0200

    Aff matrice - 020

--- li/exos/020_sudoku_local.php
+++ li/exos/020_sudoku_local.php
@@ -10,7 +10,29 @@ ob_implicit_flush(); // Pour actu x.debug ds chrome
 
 // Reconstitution Test 1
 
-// vdli($sol);
+// echo implode('', range(1, 9));
+
+$ch = '123456789456789123789123456912345678345678912678912345891234567234567891567891234';
+$m  = [];
+for ($i = 0; $i < 9; ++$i) {
+  $ls[] = substr($ch, $i * 9, 9);
+  for ($j = 0; $j < 9; ++$j) {
+    $m[$i][$j] = $ls[$i][$j];
+  }
+}
+vdli($m);
+
+  // $l = explode(' ', $v);
+  // vdli($l);
+  // foreach ($l as $n) {
+  //   echo $n.' x ';
+  // }
+  // echo '<br>';
+// vdli($ls);
+
+echo '<hr>';
+
+// echo '<div style="color:blue">0</div>';
 
 //##################################################################
 /*
