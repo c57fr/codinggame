79b3b6c9df2c3b25d8cf8c6ddc8978063b6ac0e0
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-14 17:48:51 +0200

    Prépa 020 - Sudoku Validator

---
+++ li/exos/020_sudoku_local.php
@@ -0,0 +1,27 @@
+Exo 020: Sudoku Validator.
+<hr>
+<?php
+if (!function_exists('vdli')) {
+  include '../../dev/vdli.php';
+}
+ob_implicit_flush(); // Pour actu x.debug ds chrome
+
+// https://www.codingame.com/ide/puzzle/sudoku-validator
+
+// Reconstitution Test 1
+
+// vdli($sol);
+
+//##################################################################
+/*
+
+Solution dans codding :
+
+
+
+<?php
+
+
+
+
+*/
