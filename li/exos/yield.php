5e80ab82d141102ca813a55d93108c9917e1e966
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-15 22:14:34 +0200

    Fibo avec Yield

--- li/exos/yield.php
+++ li/exos/yield.php
@@ -5,15 +5,52 @@ if (!function_exists('vdli')) {
 }
 ob_implicit_flush(); // Pour actu x.debug ds chrome
 
-function gen_one_to_three()
-{
-  for ($i = 1; $i <= 3; ++$i) {
-    // Notez que $i est préservé entre chaque production de valeur.
-    yield $i;
+$exemple = 2; // 1 to ...
+
+if (2 === $exemple) {
+  function getFibonacci()
+  {
+    $i = 0;
+    $k = 1; //first fibonacci value
+    yield $k;
+    while (true) {
+      $k = $i + $k;
+      $i = $k - $i;
+      yield $k;
+    }
+  }
+
+  $y = 0;
+
+  foreach (getFibonacci() as $fibonacci) {
+    echo $fibonacci."\n";
+    ++$y;
+    if ($y > 5) {
+      break; // infinite loop prevent
+    }
   }
 }
 
-$generator = gen_one_to_three();
-foreach ($generator as $value) {
-  echo "${value}\n";
+if (1 === $exemple) {
+  $input = <<<'EOF'
+    1;PHP;Likes dollar signs
+    2;Python;Likes whitespace
+    3;Ruby;Likes blocks
+EOF;
+
+  function input_parser($input)
+  {
+    foreach (explode("\n", $input) as $line) {
+      $fields = explode(';', $line);
+      $id     = array_shift($fields);
+
+      yield $id => $fields;
+    }
+  }
+
+  foreach (input_parser($input) as $id => $fields) {
+    echo "${id}:\n<br>";
+    echo "    {$fields[0]}\n<br>";
+    echo "    {$fields[1]}\n<hr>";
+  }
 }
