bddfe0632dcfdf17c51fedb3d55da04dc44bbe55
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-16 19:28:26 +0200

    Prépa bases

--- mo/bases/01.php
+++ mo/bases/01.php
@@ -6,14 +6,18 @@ if (!function_exists('vdli')) {
 }
 
 // http://phpdebutant.org/article14.php
-// 1 fichier par leçon (Total 24- Liens sur la droite)
+// 1 fichier par leçon complètement LUE (Total 24- Liens sur la droite)
 
 // Tu fais marcher chaque exemple recontré dans le texte lu  (copier/coller interdit)
-echo 'Bonjour le monde !' ;
+echo 'Bonjour le monde !';
 
 // Séparation + mon exemple personnel
 echo '<hr>';
-echo 'J\'affiche un texte personnel pour utiliser ce que je viens d\'apprendre' ;
+echo 'J\'affiche un texte personnel pour utiliser ce que je viens d\'apprendre<hr>';
 
 // Avant de continuer cette leçon, je fais un commit + push
 
+echo ' <font face="arial" size="5" color="red">Bonjour le monde !</font> ';
+
+// exemple perso similaire :
+// ... @ toa de jouer ;-) !
