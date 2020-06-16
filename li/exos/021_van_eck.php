4643cf653afb587afdbb25aeb8f0d8af9b3c51f9
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-17 00:22:18 +0200

    Add 2do Chrono

--- li/exos/021_van_eck.php
+++ li/exos/021_van_eck.php
@@ -18,7 +18,7 @@ $A1 = 77;
 $N  = 5e1;
 echo '<p style = "text-align:center; font-family:arial"><font size="3"><strong>'.number_format($N, 0, ',', ' ').'</strong> premiers éléments en partant de <strong>'.$A1.'</strong></font></p><hr>';
 
-include '021_ve.php'; // 2do Meilleure méthode (Suppr à terme)
+include '021_ve.php'; // 2do Meilleure actuelle  méthode (Suppr à terme)
 // include '021_ve1.php'; // Double boucle
 include '021_ve2.php'; // 2do Avec Manipulations Array
 include '021_ve3.php'; // 2do  Dictionnaire  ([N, lastIndex])
@@ -26,6 +26,7 @@ include '021_ve4.php'; // 2do Avec Yield
 // 2do Meilleure méthode -> Renvoie que le Nième elmt
 // include '021_ve5.php';
 
+// 2do Chrono + précis :
 // 2fix microtime() pour + de précisions => xx' ss" xxx
 foreach ($methode as $k => $m) {
   $deb = time();
