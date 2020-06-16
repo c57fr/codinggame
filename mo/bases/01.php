1d71ebb77be63e8d74f9e96821d12b0146cdadaf
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-16 19:09:16 +0200

    Nett

---
+++ mo/bases/01.php
@@ -0,0 +1,19 @@
+<?php
+
+ob_implicit_flush(); // Pour actu xdebug ds chrome
+if (!function_exists('vdli')) {
+  include '../../dev/vdli.php';
+}
+
+// http://phpdebutant.org/article14.php
+// 1 fichier par leçon (Total 24- Liens sur la droite)
+
+// Tu fais marcher chaque exemple recontré dans le texte lu  (copier/coller interdit)
+echo 'Bonjour le monde !' ;
+
+// Séparation + mon exemple personnel
+echo '<hr>';
+echo 'J\'affiche un texte personnel pour utiliser ce que je viens d\'apprendre' ;
+
+// Avant de continuer cette leçon, je fais un commit + push
+
