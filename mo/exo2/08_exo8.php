80c94b57672f1506e66e87a4e97dd39ee9fd0e8f
Momo <seckndanane16@gmail.com>
2020-06-20 21:36:00 +0200

    test

--- mo/exo2/08_exo8.php
+++ mo/exo2/08_exo8.php
@@ -5,27 +5,51 @@ if (!function_exists('vdli')) {
   include '../../dev/vdli.php';
 }
 
-$arr=[-7, -3, 0, 3, 5, -5];
-//echo count($arr);
-/*if($arr < 0)
-{
-  echo count($arr);
-}
-exit;*/
-$v = [];
-/*if($arr < 0)
-  {
-    echo array_count_values($arr)."<br>";
-  }
-  if($arr == 0)
-  {
-    echo array_count_values($arr)."<br>";
-  }*/
+function search_val1(){
+  $valeurs = ['negatives', 'positive', 'nulles'];
+  
+  $arr=[-7, -3, 0, 3, 5, -5];
 
-  //exit;
   $neg = 0;
   $pos = 0;
   $nulles = 0;
+
+foreach ($arr as $k => $ar) {
+  //echo $k ." = ".$ar."<br>";
+  if($ar < 0)
+  {
+    //echo $ar++."<br>";
+    $neg++;
+    //echo count($ar)."<br>";
+  }
+
+  if($ar == 0)
+  {
+    //echo $ar."<br>";
+    //echo count($ar)."<br>";
+    $nulles++;
+  }
+  if($ar > 0)
+  {
+    //echo $ar."<br>";
+    //echo count($ar)."<br>";
+    $pos++;
+  }
+}
+echo "$valeurs[0]: $neg";
+echo "<hr>";
+echo "$valeurs[2]: $nulles";
+echo "<hr>";
+echo "$valeurs[1]: $pos";
+echo "<hr>";
+}
+vdli(search_val1());
+$arr=[-7, -3, 0, 3, 5, -5];
+
+  $neg = 0;
+  $pos = 0;
+  $nulles = 0;
+
 foreach ($arr as $k => $ar) {
   //echo $k ." = ".$ar."<br>";
   if($ar < 0)
