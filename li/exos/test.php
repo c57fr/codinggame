144a74d0db6a33f5b23ed5229500a3084c5ba971
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-21 03:11:55 +0200

    ve6

--- li/exos/test.php
+++ li/exos/test.php
@@ -1,19 +1,22 @@
 <?php
 
-
-// vdli($a);
-
 $A1 = 7;
 $N  = 11;
 
-function ve6($An = 7, $N=11) // Tableau [N, index] avec yield
+function ve6($An = 7, $N = 11) // Tableau [N, index] avec yield
 {
-  foreach (range(1,$N-1) as $ii) [ $last[$An], $An ] = [ $ii, isset($last[$An]) ? $ii - $last[$An] : 0 ];
-echo $An;
-  
-  return [];
+  // $m_ini = memory_get_usage();
+  foreach (range(1, $N - 1) as $ii) {
+    [$last[$An], $An] = [$ii, isset($last[$An]) ?
+      $ii - $last[$An] :
+      0];
+  }
+  echo $An;
+    // $m_fin = memory_get_usage();
+    // $a[]   = mf($m_fin - $m_ini
+  return [$An];
 }
-$ve=ve6($A1, $N);
+$ve = ve6($A1, $N);
 echo '<hr>Der: '.end($ve);
 
 /*
