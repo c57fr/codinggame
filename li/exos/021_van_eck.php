bd6a1fed76af568882ac8e111bd0c8c5adeeb40d
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-21 02:44:29 +0200

    Les 5 ve activés (≃ 5 mn)

--- li/exos/021_van_eck.php
+++ li/exos/021_van_eck.php
@@ -12,17 +12,17 @@ $affArr = function ($arr) {
 // 0
 
 $A1 = 7; // 0 ≤ A1 ≤ 200
-$N  = 1e7; // 1 ≤ N ≤ 1000000 - // 1e4+5 => 2 958
+$N  = 5e4; // 1 ≤ N ≤ 1000000 - // 1e4+5 => 2 958
 //NB: 5e3 pour env. 1.25" / 16 kb - 1e4 env 5 pour ve1 & ve2"
-// 1e7 en ≃ 17"
+// ve5: 1e7 en ≃ 17"
 
 echo '<p style = "text-align:center; font-family:arial"><font size="3"><strong>'.number_format($N, 0, ',', ' ').'</strong> premier'.($plur = ($N > 1) ? 's' : '').' élément'.$plur.' en partant de <strong>'.$A1.'</strong></font></p><hr>';
 
-// include '021_ve1.php'; // Double boucle
-// include '021_ve2.php'; // Avec Manipulations Array
-// include '021_ve3.php'; // Dictionnaire  ([N, lastIndex])
-// include '021_ve4.php'; // Avec Yield
 include '021_ve5.php'; // Vars dynamiques
+include '021_ve3.php'; // Dictionnaire  ([N, lastIndex])
+include '021_ve4.php'; // Avec Yield
+include '021_ve2.php'; // Avec Manipulations Array
+include '021_ve1.php'; // Double boucle
 // include '021_ve6.php'; // Otpv
 // 2do Meilleure méthode -> Pour coding, ne renvoie que le Nième elmt
 // include '021_ve5.php';
