07b30e96743a81f203ac6564650749702d7bd6f1
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-21 13:27:18 +0200

    Test ve7

--- li/exos/test.php
+++ li/exos/test.php
@@ -3,21 +3,84 @@
 $A1 = 7;
 $N  = 11;
 // 2do dico des indexes - 1 boucle + ternaire
-function ve7($An = 7, $N = 11) // Tableau [N, index] avec yield
+function ve7($An = 7, $N = 11) // range
 {
-  // $m_ini = memory_get_usage();
+  $deb   = microtime(true);
+  $m_ini = memory_get_usage();
   foreach (range(1, $N - 1) as $ii) {
-    [$last[$An], $An] = [$ii, isset($last[$An]) ?
+    list($last[$An], $An) = [$ii, isset($last[$An]) ?
       $ii - $last[$An] :
-      0];
+      0, ];
   }
-  echo $An;
-    // $m_fin = memory_get_usage();
-    // $a[]   = mf($m_fin - $m_ini
-  return [$An];
+  // echo $An.'<br>';
+  $m_fin = memory_get_usage();
+  $chg   = mf($m_fin - $m_ini);
+
+  return [$An, $chg, chrono($deb, 1)];
 }
-$ve = ve7($A1, $N);
-echo '<hr>Der: '.end($ve);
+
+function ve77($An = 7, $N = 11) // for i
+{
+  $deb   = microtime(true);
+  $m_ini = memory_get_usage();
+  foreach (range(1, $N - 1) as $ii) {
+    $n = [$ii, isset($last[$An]) ?
+      $ii - $last[$An] :
+      0, ];
+  }
+  list($last[$An], $An) = $n;
+  // echo $An.'<br>';
+  $m_fin = memory_get_usage();
+  $chg   = mf($m_fin - $m_ini);
+
+  return [$An, $chg, chrono($deb, 1)];
+}
+
+function ve777($An = 7, $N = 11) // for i
+{
+  $deb   = microtime(true);
+  $m_ini = memory_get_usage();
+  // foreach (range(1, $N - 1) as $ii) {
+  for ($ii = 1; $ii < $N - 1; ++$ii) {
+    $n = [$ii, isset($last[$An]) ?
+      $ii - $last[$An] :
+      0, ];
+  }
+  list($last[$An], $An) = $n;
+  // echo $An.'<br>';
+  $m_fin = memory_get_usage();
+  $chg   = mf($m_fin - $m_ini);
+
+  return [$An, $chg, chrono($deb, 1)];
+}
+
+function ve7777($A1 = 7, $N = 11) // Vars Dyn.
+{
+  $deb   = microtime(true);
+  $m_ini = memory_get_usage();
+  $n     = $i     = $A1;
+
+  while (--$N) {
+    $v = $n;
+    // echo $v.' ';
+    $n    = isset(${$n}) ? $i - ${$n} : 0;
+    ${$v} = $i++;
+  }
+  $a      = [$n];
+  $m_fin1 = memory_get_usage();
+  $a[]    = mf($m_fin1 - $m_ini);
+  $a[]    = chrono($deb, 1);
+
+  return $a;
+}
+
+// $ve['7 - 1 boucle Range'] = ve7($A1, $N);
+$ve['77 - list() hors boucle'] = ve77($A1, $N);
+$ve['777 - 1 boucle For']      = ve777($A1, $N);
+$ve['7777 - Vars dyn.']        = ve7777($A1, $N);
+vdli($ve);
+
+// echo '<hr>Der: '.end($ve);
 
 /*
 fscanf(STDIN, "%d", $An);
