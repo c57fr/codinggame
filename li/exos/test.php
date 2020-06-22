a50f307ef414412833d3de9a51e63cac8808af7f
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-22 12:32:39 +0200

    ve7 vars scalar OK

--- li/exos/test.php
+++ li/exos/test.php
@@ -1,7 +1,7 @@
 <?php
 
 $A1 = 7;
-$N  = 11;
+$N  = 11e5;
 // 2do dico des indexes - 1 boucle + ternaire
 function ve7($An = 7, $N = 11) // range
 {
@@ -10,7 +10,7 @@ function ve7($An = 7, $N = 11) // range
   foreach (range(1, $N - 1) as $ii) {
     list($last[$An], $An) = [$ii, isset($last[$An]) ?
       $ii - $last[$An] :
-      0, ];
+      0];
   }
   // echo $An.'<br>';
   $m_fin = memory_get_usage();
@@ -23,22 +23,24 @@ function ve77($vl = 7, $N = 11) // for i
 {
   $deb   = microtime(true);
   $m_ini = memory_get_usage();
-$pl=1;
-  foreach (range(1, $N - 1) as $ii) {
-    $n = [$ii, isset($a[$vl]) ?
-      $ii - $a[$v] :
-      0, ];
-      $a[$vl]= $ii;
+  $pl    = 1;
+  $n=$vl;
+  for ($ii = 1; $ii < $N ; ++$ii) {
+    // echo $n.' ';
+    $n = (isset($a[$vl]) ?
+      $ii - $a[$vl] :
+      0);
+    $a[$vl] = $ii;
+    $vl = $n;
   }
-  $vl=$n;
   // list($last[$v], $v) = $n;
   // echo $An.'<br>';
-vdli($a);
+  // vdli($a);
 
   $m_fin = memory_get_usage();
   $chg   = mf($m_fin - $m_ini);
 
-  return [$v, $chg, chrono($deb, 1)];
+  return [$vl, $chg, chrono($deb, 1)];
 }
 
 function ve777($An = 7, $N = 11) // for i
@@ -79,10 +81,11 @@ function ve7777($A1 = 7, $N = 11) // Vars Dyn.
   return $a;
 }
 
+// 2do recherche des limites de vars dyn et scal
 $ve['7 - 1 boucle Range'] = ve7($A1, $N);
-$ve['77 - list() hors boucle'] = ve77($A1, $N);
-$ve['777 - 1 boucle For']      = ve777($A1, $N);
-$ve['7777 - Vars dyn.']        = ve7777($A1, $N);
+$ve['77 - vars scal']     = ve77($A1, $N);
+// $ve['777 - 1 boucle For']      = ve777($A1, $N);
+$ve['7777 - Vars dyn.'] = ve7777($A1, $N);
 vdli($ve);
 
 // echo '<hr>Der: '.end($ve);
