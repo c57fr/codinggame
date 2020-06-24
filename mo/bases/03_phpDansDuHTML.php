d4b4ff54391b60cb0b59c1b99f6a414c9d8962c5
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-24 13:20:57 +0200

    Up

--- mo/bases/03_phpDansDuHTML.php
+++ mo/bases/03_phpDansDuHTML.php
@@ -1,13 +1,14 @@
 <html>
 <body>
 <font size="2" face="Arial">Le texte en HTML</font>
+<!-- 2do Mo Ex perso (Choisir autre style + autre option de format de date) -->
 
 <?php
 // le code PHP -----
 $heure = date("h:i");
 print("<font size=\"2\" face=\"Arial\"> et celui en PHP.</font>");
 ?>
-<!-- retour qu code HTML   -->
+<!-- Retour au code HTML   -->
 <br><font size="2" face="Arial">Il est <?php echo $heure ;?>
 
 </font>
