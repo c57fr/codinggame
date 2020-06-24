d889bd7ae5a9bcbfc7aeaa194b96e696e33407c7
Momo <seckndanane16@gmail.com>
2020-06-24 19:59:35 +0200

    correction de l'exo 2

--- mo/bases/02_affichageDateEtHeure.php
+++ mo/bases/02_affichageDateEtHeure.php
@@ -5,3 +5,8 @@ $heure = date('H:i');
 echo "Nous sommes le ${date} et il est ${heure}";
 
 // Ex perso (Choisir autre option de format) :
+echo '<hr>';
+$jour = date('D');
+$date = date('d-m-Y');
+$heure = date('h:i a');
+echo "Nous sommes le $jour $date et il est $heure";
