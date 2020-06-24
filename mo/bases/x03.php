19598d492eb98826704415e0c057e06f820bf8d5
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-24 22:51:36 +0200

    Nett

--- mo/bases/x03.php
+++ mo/bases/x03.php
@@ -2,7 +2,7 @@
     <body>
         <font size="2" face="Arial">Le texte en HTML</font>
         <?php
-            include 'toto.inc.php';
+            include 'xtoto.inc.php';
             // on appelle le fichier
         ?>
     </body>
