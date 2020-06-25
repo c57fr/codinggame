83cb23c5a04499a599fe7b0ce5e4315cb9d057cb
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-25 20:58:35 +0200

    Calcul arrivée

--- li/exos/023_childsplay.php
+++ li/exos/023_childsplay.php
@@ -3,35 +3,66 @@ Exo 023: Child's play.<hr>
 
 // echo nf(pow(2, 53)); // n max
 
+// $w = 12; $h = 6; $n = 987;
+// $p = 41;
+// echo($p % $w).' '.intdiv($p, $w)."\n";
 // exit;
 // Reconstitution T1
-$w  = 12; $h  = 6; $n  = 987;
+$w = 12; $h = 6; $n = 987;
 // $n  = $ni  = 77;
 $ch = <<<'EOD'
 ...#........
 ...........#
 ............
 ............
-..#0........
+..#O........
 ..........#.
 EOD;
 // => 7 1
 
+
+// Reconstitution T2
+$w  = 12; $h  = 8; $n  = 1234321;
+// $n  = $ni  = 77;
+$ch = <<<'EOD'
+....#.......
+........#...
+...........#
+...#O.......
+...#........
+.......#....
+...........#
+....#.......
+EOD;
+// => 4 2
+
+
 $chAff = str_replace("\r\n", '<br>', $ch);
 $ch    = str_replace("\r\n", '', $ch);
 
-$p = strpos($ch, '0'); // Position
+$p = strpos($ch, 'O'); // Position
+
+echo $loopfound = $w*$h- substr_count($ch, '#');
 
 $s  = [$p - $w, $p + 1, $p + $w, $p - 1]; // Sens
 $vs = 0;
+$r=[];
 
-while (--$n) {
+while (--$loopfound) {
   $p = $s[$vs % 4];
   $s = [$p - $w, $p + 1, $p + $w, $p - 1]; // Sens
   $vs += '#' === ($ch[$s[$vs % 4]]);
-}
-echo ($p%$w+1).' '.intdiv($p, $w)."\n";
+  
+  $r[$p]=($r[$p]?? 0) +1; // Route
+  // $r[$p]=$p;
 
+}
+vdli($r);
+vdli(count($r));
+vdli($p);
+echo($p % $w).' '.intdiv($p, $w)."\n<hr>";
+
+echo ($n-$loopfound-1) % count($r);
 
 // https://www.codingame.com/ide/puzzle/a-childs-play
 //##################################################################
--- li/exos/023_childsplay_cdg.php
+++ li/exos/023_childsplay_cdg.php
@@ -2,28 +2,31 @@
 
 fscanf(STDIN, '%d %d', $w, $h);
 fscanf(STDIN, '%d', $n);
-error_log(var_export($w.' x '.$h.' - '.$n, true));
+error_log(var_export($w.' x '.$h.' - '.number_format($n, 0, ', ', ' '), true));
 $ch = '';
 for ($i = 0; $i < $h; ++$i) {
-  // $ch += stream_get_line(STDIN, 500 + 1, "\n");
-  // $ch = stream_get_line(STDIN, 500 + 1);
-  $ch .= trim(fgets(STDIN));
+  $str = trim(fgets(STDIN));
+  $row = $str;
+  $ch.=$str;
+  error_log(var_export($row, true));
+  // $ch .= stream_get_line(STDIN, 500 + 1);
 }
+
 //   error_log(var_export(strlen($ch), true));
 function vd($v)
 {
   return error_log(var_export($v, true));
 }
 
-$p = strpos($ch, 'O'); // Position
+$p = stripos($ch, 'O'); // Position
 vd($p);
 
 $s  = [$p - $w, $p + 1, $p + $w, $p - 1]; // Sens
 $vs = 0;
 
-while (--$n) {
+while ($n--) {
   $p = $s[$vs % 4];
   $s = [$p - $w, $p + 1, $p + $w, $p - 1]; // Sens
   $vs += '#' === ($ch[$s[$vs % 4]]);
 }
-echo($p % $w + 1).' '.intdiv($p, $w)."\n";
+echo($p % $w).' '.intdiv($p, $w)."\n";
