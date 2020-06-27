cb6c77857437a54b3c9c0e89213b77fa96ad5879
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-27 22:27:15 +0200

    Prépa 025 - Unit Fractions

---
+++ li/exos/025_unitFractions.php
@@ -0,0 +1,12 @@
+Exo 025: Unit Fractions.<hr>
+<?php
+
+// Reconstitution T1
+
+// Reconstitution T2
+
+// Reconstitution T3
+
+// https://www.codingame.com/ide/puzzle/create-the-longest-sequence-of-1s
+//##################################################################
+// Solution validée dans codding :
---
+++ li/exos/025_unitFractions_cdg.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+fscanf(STDIN, "%d", $n);
+
+// Write an answer using echo(). DON'T FORGET THE TRAILING \n
+// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+echo("1/n = 1/x + 1/y\n");
+?>
