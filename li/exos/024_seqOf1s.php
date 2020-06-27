679f0c11e0118ca07daa5fc94bf7afe1ab6e1b96
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-27 21:50:46 +0200

    Soluce 024 (cdg)

--- li/exos/024_seqOf1s.php
+++ li/exos/024_seqOf1s.php
@@ -3,53 +3,49 @@ Exo 024: Create the longest sequence of 1s.<hr>
 // $s = 'x';
 
 // Reconstitution T1
-// $b='00';
+$b='00';
 // echo "1\n";
 
 // Reconstitution T2
-// $b = '01010';
+$b = '01010';
 // echo "3\n";
 
 // Reconstitution T3
-$b = '11011101111';
+// $b = '11011101111';
+// $b='11011111111';
+// 8
+
+
 
 preg_match_all('/0/', $b, $z, PREG_OFFSET_CAPTURE);
+$l = strlen($b);
+$a = array_column($z[0], 1);
+$a[] = $l;
 
-vdli(array_column($z[0],1));
+$max = 0;
+foreach ($a as $k => $v) {
+  $max = max($max, ($a[$k + 1] ?? $l) - ($a[$k - 1] ?? -1) - 1);
+}
+echo $max."\n";
 
+////////////////////////////////////////
+
+preg_match_all('/0/', $b, $z, PREG_OFFSET_CAPTURE);
+vdli($a = array_column($z[0], 1));
+vdli($l = strlen($b));
+$a[] = $l;
+
+vdli($a);
 echo '<hr>';
 
-// $b2='11011111111';
-// 8
-vdli($l = strlen($b));
-
-$b .= '0';
-for ($ip = 0, $l = strlen($b), $i = 0; $i < $l; ++$i) {
-  if (!$b[$i]) {
-    $a[] = $i; // + index précédent
-    // echo vf($i).' '.vf($i - $ip,5).'<br>';
-    // $ip = $i; // last index
-  }
-//   ++$n;
+$max = 0;
+foreach ($a as $k => $v) {
+  $max = max($max, ($a[$k + 1] ?? $l) - ($a[$k - 1] ?? -1) - 1);
 }
-// vdli($a);
-// // $max=0;
-// foreach ($a as $k => $v) {
-//   echo vf($k,15).' '.$v.'<br>';
-// }
-// echo '<hr>'.$v."\n";
-vdli($a);
 
-$s = [$a[0]];
-for ($l = count($a),$i = 1; $i < $l; ++$i) {
-  $s[$i] = $a[$i] - $a[$i - 1] + $s[$i - 1];
-  vdli($a);
-  vdli($s);
-  echo $i.'<hr>';
-}
-// vdli($s);
+echo $max.'<hr>';
 
-echo '<hr>'.$i."\n";
+
 
 // https://www.codingame.com/ide/puzzle/create-the-longest-sequence-of-1s
 //##################################################################
--- li/exos/024_seqOf1s_cdg.php
+++ li/exos/024_seqOf1s_cdg.php
@@ -2,21 +2,13 @@
 
 $b = stream_get_line(STDIN, 999 + 1, "\n");
 
-$s = 'x';
-// Reconstitution T1
-// $b='00';
-error_log(var_export($b, true));
-echo "1\n";
+preg_match_all('/0/', $b, $z, PREG_OFFSET_CAPTURE);
+$l   = strlen($b);
+$a   = array_column($z[0], 1);
+$a[] = $l;
 
-// Reconstitution T2
-// $b='01010';
-error_log(var_export($b, true));
-echo "3\n";
-
-// Reconstitution T3
-// $b='11011101111';
-// $b2='11011111111';
-// 8
-error_log(var_export($b, true));
-
-
+$max = 0;
+foreach ($a as $k => $v) {
+  $max = max($max, ($a[$k + 1] ?? $l) - ($a[$k - 1] ?? -1) - 1);
+}
+echo $max."\n";
