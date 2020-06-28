ac95d2acc285e097e95a476b637dffa88c5a8540
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-28 14:51:23 +0200

    Soluce Pneu84

--- li/exos/025_unitFractions_cdg.php
+++ li/exos/025_unitFractions_cdg.php
@@ -8,3 +8,11 @@ for ($l = $n * 2 + 1, $y = $n + 1; $y < $l; ++$y) {
     echo '1/'.$n.' = 1/'.$x.' + 1/'.$y."\n";
   }
 }
+
+
+// Soluce Pneu84
+/*
+fscanf(STDIN, "%d", $n);
+$nn = $n * $n;
+for ($i = 1; $i <= $n; $i++) if ($nn % $i == 0) echo "1/$n = 1/" . ($n + $nn / $i) . " + 1/" . ($n + $i) . "\n"; 
+*/
