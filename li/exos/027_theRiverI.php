6b940f0b823184695ce70c0a24323352e12d1493
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-29 18:08:57 +0200

    Opti 026

--- li/exos/027_theRiverI.php
+++ li/exos/027_theRiverI.php
@@ -14,6 +14,14 @@ if (getenv('local')) {
 
   // Reconstitution T3
 
+  while ($r1 !== $r2) {
+    $v = $r1 < $r2 ? 'r1' : 'r2';
+    ${$v} += array_sum(str_split((string) ${$v}));
+  }
+
+  echo $r1;
+
+/*
   function nxt($n)
   {
     // $n = (string) $n;
