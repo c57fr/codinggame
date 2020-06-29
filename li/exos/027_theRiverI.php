173b00a4cd32e86bcebfdad3a8527a8d8a8de0b5
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-29 18:01:16 +0200

    Soluce 027

--- li/exos/027_theRiverI.php
+++ li/exos/027_theRiverI.php
@@ -5,7 +5,7 @@ if (getenv('local')) {
 
   // Reconstitution T1
   $r1 = '32';
-  $r2 = 47;
+  $r2 = '47';
 
   // var_dump($r1);
   // var_dump((string) $r2);
@@ -13,38 +13,49 @@ if (getenv('local')) {
   // Reconstitution T2
 
   // Reconstitution T3
-  function sumd($n)
+
+  function nxt($n)
   {
-    $n = (string) $n;
+    // $n = (string) $n;
     for ($sum = 0, $l = strlen($n), $i = 0; $i < $l; ++$i) {
       $sum += (int) $n[$i];
     }
 
-    return $sum;
+    return $n + $sum;
   }
 
-  $ni = 0;
-  // for ($i = 9; $i < 1e2; ++$i) {
-  //   $s = sum(strval($i));
-  //   echo vf($i).' '.vf($s).'<br>';
-  // }
+  $a = [$r1, $r2];
 
-  $n = 123456;
-  function nxt($n)
-  {
-    return $n + sumd($n);
+  while ($a[0] !== $a[1]) {
+    sort($a);
+    var_dump($a);
+    echo($a[0] = (string) nxt($a[0])).'<hr>';
   }
+  echo $a[0]."\n";
 
-  $t = 21;
-  while ($t--) {
-    $deb = microtime(true);
-    $i   = 7e5;
-    for ($i = 7e4; $i; --$i) {
-      $sum = sumd($n);
-    }
-    echo  $n.' '.$sum.' '.nxt($n).' '.chrono($deb, 1).'<hr>';
-    // sleep(1);
+// echo $a.' '.$b.' '.($a <=> $b).'<br>';
+  // $mn = min($a, $b);
+
+  // echo nxt($mn);
+
+/*
+$n = 123456;
+function nxt($n)
+{
+  return $n + sumd($n);
+}
+
+$t = 21;
+while ($t--) {
+  $deb = microtime(true);
+  $i   = 7e5;
+  for ($i = 7e4; $i; --$i) {
+    $sum = sumd($n);
   }
+  echo  $n.' '.$sum.' '.nxt($n).' '.chrono($deb, 1).'<hr>';
+  // sleep(1);
+}
+*/
 }
 
 // https://www.codingame.com/training/easy/the-river-i-
@@ -56,5 +67,21 @@ else {
   fscanf(STDIN, '%d', $r1);
   fscanf(STDIN, '%d', $r2);
   error_log(var_export($r1.' '.$r2, true));
-  echo "47\n";
+
+  function nxt($n)
+  {
+    for ($sum = 0, $l = strlen($n), $i = 0; $i < $l; ++$i) {
+      $sum += (int) $n[$i];
+    }
+
+    return $n + $sum;
+  }
+
+  $a = [$r1, $r2];
+
+  do {
+    sort($a);
+    $a[0] = (string) nxt($a[0]);
+  } while ($a[0] !== $a[1]);
+  echo $a[0]."\n";
 }
