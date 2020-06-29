9df0a0e52a2e22dee31893c35ead2d40455441aa
Momo <seckndanane16@gmail.com>
2020-06-30 00:20:26 +0200

    exo 11: create, alter et drop sql/mysql

---
+++ mo/bases/phpdebutant.sql
@@ -0,0 +1,4 @@
+CREATE TABLE clients_tbl(id INT NOT NULL AUTO_INCREMENT, prenom VARCHAR (50) NOT NULL, nom VARCHAR (50) NOT NULL, ne_le DATE NOT NULL, ville VARCHAR (90) NOT NULL, enfants INT NOT NULL, PRIMARY KEY (id))phpdebutantclients_tblclients_tblclients_tbl
+ALTER TABLE clients_tbl ADD tel INT NOT NULL
+ALTER TABLE clients_tbl DROP tel
+DROP TABLE clients_tbl
