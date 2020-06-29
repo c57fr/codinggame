29fd8772a90b94780220dbfad2c8c024be2da82a
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-30 01:10:50 +0200

    Up 029

--- li/exos/029_bushfire.php
+++ li/exos/029_bushfire.php
@@ -3,12 +3,10 @@
 if (getenv('local')) {
   echo '029 - 1D Bush Fire.<hr>';
 
+  // Reconstitution T1 //
 }
 // https://www.codingame.com/ide/puzzle/1d-bush-fire
 //##################################################################
 // https://github.com/c57fr/codinggame
 
 // Solution validée dans codding :
-else {
-  
-}
