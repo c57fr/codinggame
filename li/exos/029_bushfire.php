f0442caa1693b255724718b79a9b8140da852297
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-30 01:04:06 +0200

    Prépa 029 - 1D Bush Fire

---
+++ li/exos/029_bushfire.php
@@ -0,0 +1,14 @@
+<?php
+
+if (getenv('local')) {
+  echo '029 - 1D Bush Fire.<hr>';
+
+}
+// https://www.codingame.com/ide/puzzle/1d-bush-fire
+//##################################################################
+// https://github.com/c57fr/codinggame
+
+// Solution validée dans codding :
+else {
+  
+}
