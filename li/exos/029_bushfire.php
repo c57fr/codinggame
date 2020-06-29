245fbc12c513b763cfdc0012701d69174032e86d
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-30 01:32:43 +0200

    Add code cdg 026

--- li/exos/029_bushfire.php
+++ li/exos/029_bushfire.php
@@ -3,10 +3,23 @@
 if (getenv('local')) {
   echo '029 - 1D Bush Fire.<hr>';
 
-  // Reconstitution T1 //
+// Reconstitution T1 //
 }
 // https://www.codingame.com/ide/puzzle/1d-bush-fire
 //##################################################################
 // https://github.com/c57fr/codinggame
 
 // Solution validée dans codding :
+
+else {
+  fscanf(STDIN, '%d', $N);
+  for ($i = 0; $i < $N; ++$i) {
+    $line = stream_get_line(STDIN, 255 + 1, "\n");
+  }
+  for ($i = 0; $i < $N; ++$i) {
+    // Write an answer using echo(). DON'T FORGET THE TRAILING \n
+    // To debug: error_log(var_export($var, true)); (equivalent to var_dump)
+
+    echo "answer\n";
+  }
+}
