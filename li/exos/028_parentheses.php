0f3ca61deeb8d261046402d71c9814afcdd4d9f3
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-29 23:02:37 +0200

    Opti soluce 1 - 028

--- li/exos/028_parentheses.php
+++ li/exos/028_parentheses.php
@@ -9,26 +9,41 @@ if (getenv('local')) {
   // Reconstitution T2
   // $ch = '{([{S}]]6K[()]}';
 
-  // Reconstitution T3
+  // T3
+  // $ch = 'W12{}{}L{}';
+  // Reconstitution Autre
+  // $ch = 'abc]';
+
+  vdli($ch);
+  $ch = preg_replace('/[^\(^\)^\{^\}^\[^\]]/', '', $ch);
+  vdli($ch);
 
   $pairs = ['(' => ')', '[' => ']', '{' => '}'];
   vdli($pairs);
+  echo '<p style="font-size:20px; letter-spacing: 1rem;">'.$ch.'</p>';
   $ders = [];
-  $res  = 'true';
-  for ($l = strlen($ch),$i = -1; $res && ++$i < $l;) {
-    echo $i.' - '.implode(' ', $ders).'<br>';
-    if (array_key_exists($ch[$i], $pairs)) {
+  $res  = true;
+
+  for ($l = strlen($ch),$i = -1; $res && (++$i < $l);) {
+    echo $ch[$i].' - '.implode(' ', $ders).'<br>';
+
+    if (array_key_exists($ch[$i], $pairs)) { // entrant
       $ders[] = $pairs[$ch[$i]];
+      $res    = true;
     }
-    if (in_array($ch[$i], $pairs, true) && $ch[$i] === end($ders)) {
-      array_pop($ders);
+    if (in_array($ch[$i], $pairs, true)) {
+      if ($ch[$i] === end($ders)) { // fermant
+        array_pop($ders);
+      } else {
+        $res = false;
+      }
     }
   }
-  if ($ders) {
-    $res = 'false';
+  if (!$res || $ders) {
+    $res = false;
   }
 
-  echo $res."\n";
+  echo(($res) ? 'true' : 'false')."\n";
 }
 // https://www.codingame.com/ide/puzzle/brackets-extreme-edition
 //##################################################################
@@ -39,19 +54,29 @@ else {
   fscanf(STDIN, '%s', $ch);
   error_log(var_export($ch, true));
 
+  $ch = preg_replace('/[^\(^\)^\{^\}^\[^\]]/', '', $ch);
+
   $pairs = ['(' => ')', '[' => ']', '{' => '}'];
   $ders  = [];
-  $res   = 'true';
-  for ($l = strlen($ch),$i = -1; $res && ++$i < $l;) {
-    if (array_key_exists($ch[$i], $pairs)) {
+  $res   = true;
+
+  for ($l = strlen($ch),$i = -1; $res && (++$i < $l);) {
+    if (array_key_exists($ch[$i], $pairs)) { // entrant
       $ders[] = $pairs[$ch[$i]];
+      $res    = true;
     }
-    if (in_array($ch[$i], $pairs, true) && $ch[$i] === end($ders)) {
-      array_pop($ders);
+    if (in_array($ch[$i], $pairs, true)) {
+      if ($ch[$i] === end($ders)) { // fermant
+        array_pop($ders);
+      } else {
+        $res = false;
+      }
     }
+    // if ($i==0 && in_array($ch[$i], $pairs, true));
   }
-  if (!empty($ders)) {
-    $res = 'false';
+  if (!$res || $ders) {
+    $res = false;
   }
-  echo $res."\n";
+
+  echo(($res) ? 'true' : 'false')."\n";
 }
