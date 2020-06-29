67f3156488fa0c6bb8c811895fde4c1481987bd3
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-29 21:32:38 +0200

    Opti soluce 1

--- li/exos/028_parentheses.php
+++ li/exos/028_parentheses.php
@@ -15,21 +15,18 @@ if (getenv('local')) {
   vdli($pairs);
   $ders = [];
   $res  = 'true';
-  for ($l = strlen($ch),$i = 0; $i < $l && $res; ++$i) {
-    // echo 'Ini.: '.implode(' ', $ders).'<br>';
-    // echo $ch[$i].' ';
+  for ($l = strlen($ch),$i = -1; $res && ++$i < $l;) {
+    echo $i.' - '.implode(' ', $ders).'<br>';
     if (array_key_exists($ch[$i], $pairs)) {
       $ders[] = $pairs[$ch[$i]];
-      // echo 'Ouvr.: '.implode(' ', $ders).'<br>';
     }
     if (in_array($ch[$i], $pairs, true) && $ch[$i] === end($ders)) {
       array_pop($ders);
-    // echo 'Ferm.: '.$ch[$i].' - '.implode(' ', $ders).'<br>';
-    } elseif (in_array($ch[$i], $pairs, true) && $ch[$i] !== end($ders)) {
-      $res = false;
     }
   }
-  if(!empty($ders)) $res="false";
+  if ($ders) {
+    $res = 'false';
+  }
 
   echo $res."\n";
 }
@@ -43,18 +40,18 @@ else {
   error_log(var_export($ch, true));
 
   $pairs = ['(' => ')', '[' => ']', '{' => '}'];
-  $ders = [];
-  $res  = 'true';
-  for ($l = strlen($ch),$i = 0; $i < $l && $res; ++$i) {
+  $ders  = [];
+  $res   = 'true';
+  for ($l = strlen($ch),$i = -1; $res && ++$i < $l;) {
     if (array_key_exists($ch[$i], $pairs)) {
       $ders[] = $pairs[$ch[$i]];
     }
     if (in_array($ch[$i], $pairs, true) && $ch[$i] === end($ders)) {
       array_pop($ders);
-    } elseif (in_array($ch[$i], $pairs, true) && $ch[$i] !== end($ders)) {
-      $res = 'false';
     }
   }
-  if(!empty($ders)) $res="false";
+  if (!empty($ders)) {
+    $res = 'false';
+  }
   echo $res."\n";
 }
