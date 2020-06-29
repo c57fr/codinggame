0bd7725f9707f73d0b24a60711540d70059d92c8
GrCOTE7 <GrCOTE7@GMail.com>
2020-06-29 14:09:17 +0200

    Prépa 027 - The Rver 1

---
+++ li/exos/027_theRiver1.php
@@ -0,0 +1,16 @@
+<?php
+
+if (getenv('local')) {
+  echo '027 - The River1.<hr>';
+
+  // Reconstitution T1
+
+  // Reconstitution T2
+
+// Reconstitution T3
+}
+// https://www.codingame.com/training/easy/the-river-i-
+//##################################################################
+// https://github.com/c57fr/codinggame
+
+// Solution validée dans codding :
