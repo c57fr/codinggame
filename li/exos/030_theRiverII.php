2f5fe7414433192afe2ffae34ea4943cb5b586ab
GrCOTE7 <GrCOTE7@GMail.com>
2020-07-01 05:54:17 +0200

    Exemples

--- li/exos/030_theRiverII.php
+++ li/exos/030_theRiverII.php
@@ -5,12 +5,14 @@ if (getenv('local')) {
 
   // Reconstitution T //
 $r1 = 80; // Y
+$r1 = 13; // Y
+$r1 = 20; // Y
 
 $nxt = $r1 + array_sum(str_split((string) $r1));
   vdli($nxt);
   // recherche précédent
   for ($i = $r1; $i > 0; --$i) {
-    echo $i.' '.($i + array_sum(str_split((string) $r1))).'<br>';
+    echo $i.' '.($i + array_sum(str_split((string) $i))).'<br>';
   }
 }
 //*** https://www.codingame.com/training/easy/the-river-ii-
