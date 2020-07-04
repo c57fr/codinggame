fa672898a17cd9c995d0c5c6596f9eab64682e47
GrCOTE7 <grcote7@gmail.com>
2020-07-04 02:10:57 +0200

    Corr 014

--- mo/bases/14_where.php
+++ mo/bases/14_where.php
@@ -1,18 +1,30 @@
-<select name="genre">
+<!-- <select name="genre"> -->
 <?php
-mysql_connect($server,$user,$pass) OR die('Erreur de connexion');
-mysql_select_db($db) OR die('Base inexistante');
-$sql = 'SELECT * from genres;';
-$query = mysql_query($sql) OR die('Erreur');
-$nb = mysql_num_rows($query);
-if(!$nb[0])
-{
-    echo '<option>Aucun genre</option>';
-}else{
-    while($list = mysql_fetch_array($query))
-    {
-        echo '<option value="'.$list['CodeGenre'].'">'.$list['LibelleGenre'].'<option>';
-    }
+$server = 'localhost';
+$user   = 'root';
+$pw     = '';
+$db     = 'codingame';
+$mysqli = new mysqli($server, $user, $pw, $db);
+if (!$mysqli->set_charset('utf8')) {
+  printf("Erreur lors du chargement du jeu de caractères utf8 : %s\n", $mysqli->error);
+  exit();
 }
-mysql_close();
-?> 
+  printf("Jeu de caractères courant : %s\n", $mysqli->character_set_name());
+
+if ($mysqli->connect_errno) {
+  echo 'Echec lors de la connexion à MySQL : ('.$mysqli->connect_errno.') '.$mysqli->connect_error;
+}
+$sql = 'SELECT * from genres';
+$req = $mysqli->query($sql);
+$row = $req->fetch_assoc();
+var_dump($row);
+
+exit;
+$nb = mysqli_num_rows($query);
+if (!$nb[0]) {
+  echo '<option>Aucun genre</option>';
+} else {
+  while ($list = mysqli_fetch_array($query)) {
+    echo '<option value="'.$list['CodeGenre'].'">'.$list['LibelleGenre'].'<option>';
+  }
+}
