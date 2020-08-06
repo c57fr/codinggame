ff99a00e27d862c9ac7e42f0daec4226e89c02aa
GrCOTE7 <GrCOTE7@GMail.com>
2020-08-07 00:16:28 +0200

    Je laisse le message du commit

--- mo/index_dev.php
+++ mo/index_dev.php
@@ -9,6 +9,9 @@
     <?php
       include '../dev/vdli.php'; // vdli()
       // include 'exos/01_exo.php'; // à faire
+
+// je choisis là un fichier au hazard ds lequel ke fias une modis... Du coup, look la flèche !
+
       // include 'exos/02_exo.php'; // à faire
       include 'exos/03_laDecente.php'; // En cours
     ?>
