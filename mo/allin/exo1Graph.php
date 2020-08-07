b864d958e4ea320ca000ceed768509d68fddaf81
Momo <seckndanane16@gmail.com>
2020-08-07 02:54:57 +0200

    modif du premier commit avec quelques tâches à faire pour demain

--- mo/allin/exo1Graph.php
+++ mo/allin/exo1Graph.php
@@ -1,3 +1,11 @@
 
 exo:
 voici le tout premier commit fait grace aux explications de lio
+
+------------------------------
+je dois tous les jours m'entrainer avec git graph afin de le maitriser
+
+les tâches à faire pour demain:
+    1- je continue le tutoriel pour comprendre et maitriser prestashop
+    2- je fais un commit avec push sur git graph
+    3- j'essaie de comprendre la technique de mise à niveau sur git graph
