55379b6b2dd2eb2018b7a031c915ef43b0af0399
Momo <seckndanane16@gmail.com>
2020-08-09 02:52:02 +0200

    tâche du troisième jour

--- mo/allin/exo1Graph.php
+++ mo/allin/exo1Graph.php
@@ -14,3 +14,6 @@ les tâches à faire pour demain:
     Par rapport aux taches que je me suis fixé, je fais un commit et un push grâce git graph. 
     C'est dans le but de maitriser le git graph 
     le samedi 08/08/20 je continue le tutoriel de prestashop
+    
+    ----------------------------
+    suite de l'exo pour bien maitriser le git graph 
